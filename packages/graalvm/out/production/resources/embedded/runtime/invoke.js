c8f135dc0c866ca9081836fd74fa1393f23c0d7c
Sam Gammon <sam@dyme.com>
2022-06-24 19:23:45 -0700

    Drop ephemeral assets in git

--- packages/graalvm/out/production/resources/embedded/runtime/invoke.js
+++
@@ -1,8 +0,0 @@
-/**
- * Invoke the user's `renderContent` entrypoint function after setting up runtime state.
- */
-function embeddedExecute() {
-    // noinspection JSUnresolvedFunction,JSUnresolvedVariable
-    return embedded.renderContent;
-}
-embeddedExecute();
--- packages/graalvm/out/test/resources/embedded/simple.js
+++
@@ -1,5 +0,0 @@
-
-// noinspection JSUnusedGlobalSymbols
-function renderContent(name) {
-    return `Hello, ${name || 'embedded JS'}!`;
-}

d2e9adfd854253d36765b804c2499a6a9cbec668
Sam Gammon <sam@dyme.com>
2022-06-30 20:18:57 -0700

    Add top-level task to resolve all deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -175,3 +175,25 @@ tasks.register("resolveAndLockAll") {
 tasks.named<HtmlDependencyReportTask>("htmlDependencyReport") {
   projects = project.allprojects
 }
+
+if (tasks.findByName("resolveAllDependencies") == null) {
+  tasks.register("resolveAllDependencies") {
+    val npmInstall = tasks.findByName("kotlinNpmInstall")
+    if (npmInstall != null) {
+      dependsOn(npmInstall)
+    }
+    doLast {
+      allprojects {
+        configurations.forEach { c ->
+          if (c.isCanBeResolved) {
+            println("Downloading dependencies for '$path' - ${c.name}")
+            val result = c.incoming.artifactView { lenient(true) }.artifacts
+            result.failures.forEach {
+              println("- Ignoring Error: ${it.message}")
+            }
+          }
+        }
+      }
+    }
+  }
+}

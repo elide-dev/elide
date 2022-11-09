eb98b0f8e57e2160c4c9ab91f6b60bda5f023781
Sam Gammon <sam@dyme.com>
2022-11-08 23:41:13 -0800

    Upgrade Kotlin -> `1.7.21`
    - Upgrade Kotlin
    - Issue plugin release -> `1.0.0-beta9`
    - Issue library release -> `rc31`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,4 +1,4 @@
-val kotlinVersion = "1.7.20"
+val kotlinVersion = "1.7.21"
 
 plugins {
   `kotlin-dsl`
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 
 /** Declares common build script versions. */
 object Versions {
-  const val kotlin = "1.7.20"
+  const val kotlin = "1.7.21"
   const val antJUnit = "1.10.12"
 }

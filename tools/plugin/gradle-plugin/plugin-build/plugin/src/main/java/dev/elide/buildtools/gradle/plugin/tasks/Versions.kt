ad3b0a496237a73bddc2ac8e4581ac57e9959684
Sam Gammon <sam@dyme.com>
2022-11-23 11:53:56 -0800

    chore: upgrade esbuild -> `0.15.15`

--- tools/plugin/gradle-plugin/plugin-build/plugin/src/main/java/dev/elide/buildtools/gradle/plugin/tasks/Versions.kt
+++ tools/plugin/gradle-plugin/plugin-build/plugin/src/main/java/dev/elide/buildtools/gradle/plugin/tasks/Versions.kt
@@ -3,7 +3,7 @@ package dev.elide.buildtools.gradle.plugin.tasks
 /** Versions of build tools depended on by the embedded JS build task. */
 public object Versions {
     /** Embedded `esbuild` default version. */
-    public const val esbuild: String = "0.15.14"
+    public const val esbuild: String = "0.15.15"
 
     /** Embedded `prepack` default version. */
     public const val prepack: String = "0.2.54"

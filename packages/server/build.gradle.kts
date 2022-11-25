acb463639174ef555a7c37eaa4bd105477b0c1a1
Sam Gammon <sam@dyme.com>
2022-11-25 13:12:15 -0800

    fix: cleanup dependencies for server package

--- packages/server/build.gradle.kts
+++ packages/server/build.gradle.kts
@@ -35,12 +35,12 @@ micronaut {
 dependencies {
   // API Deps
   api(libs.jakarta.inject)
-  api(libs.slf4j)
   api(libs.graalvm.sdk)
 
   // Modules
   kapt(libs.micronaut.inject)
   kapt(libs.micronaut.inject.java)
+  implementation(libs.slf4j)
   implementation(project(":packages:base"))
   implementation(project(":packages:proto"))
 
@@ -75,20 +75,6 @@ dependencies {
   implementation(libs.brotli.native.linux)
   implementation(libs.brotli.native.windows)
 
-  // Google
-  implementation(libs.grpc.core)
-  implementation(libs.grpc.api)
-  implementation(libs.grpc.auth)
-  implementation(libs.grpc.stub)
-  implementation(libs.grpc.services)
-  implementation(libs.grpc.netty)
-  implementation(libs.grpc.protobuf)
-  implementation(libs.grpc.kotlin.stub)
-  implementation(libs.guava)
-  implementation(libs.gax.java)
-  implementation(libs.gax.java.grpc)
-  implementation(libs.google.api.common)
-
   // Micronaut
   implementation(libs.micronaut.http)
   implementation(libs.micronaut.http.server)
@@ -97,9 +83,6 @@ dependencies {
   implementation(libs.micronaut.inject)
   implementation(libs.micronaut.inject.java)
   implementation(libs.micronaut.protobuf)
-  implementation(libs.micronaut.grpc.runtime)
-  implementation(libs.micronaut.grpc.client.runtime)
-  implementation(libs.micronaut.grpc.server.runtime)
   implementation(libs.micronaut.management)
   implementation(libs.micronaut.views.core)
 

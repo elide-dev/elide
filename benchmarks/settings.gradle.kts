7b00975e3da913b9f6b13e4353b747c7ffa7d490
Sam Gammon <samuel.gammon@gmail.com>
2022-12-11 01:00:17 -0800

    fix: add kotlinx-html repo to benchmarks sub-project

--- benchmarks/settings.gradle.kts
+++ benchmarks/settings.gradle.kts
@@ -7,6 +7,7 @@ pluginManagement {
     gradlePluginPortal()
     mavenCentral()
     maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
   }
 }
 

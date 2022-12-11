99d17ef5e11e61f856fc1366d3728ef3d07595e5
Sam Gammon <samuel.gammon@gmail.com>
2022-12-11 01:17:54 -0800

    fix: kotlinx-html is unreleased at version `0.8.1`
    - Restore version `0.8.0`
    - Fix added repository for benchmarks

--- benchmarks/settings.gradle.kts
+++ benchmarks/settings.gradle.kts
@@ -7,7 +7,6 @@ pluginManagement {
     gradlePluginPortal()
     mavenCentral()
     maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
-    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
   }
 }
 
@@ -23,6 +22,7 @@ dependencyResolutionManagement {
     mavenCentral()
     maven("https://plugins.gradle.org/m2/")
     maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
   }
   versionCatalogs {
     create("libs") {

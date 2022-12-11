03082c0afe06d2e8d54432543a07645b6ed01f9c
Sam Gammon <samuel.gammon@gmail.com>
2022-12-11 00:56:32 -0800

    chore: remove old split RPC modules

--- packages/rpc-jvm/src/main/kotlin/elide/server/rpc/web/GrpcWebTypes.kt
+++
@@ -1,7 +0,0 @@
-package elide.server.rpc.web
-
-/**
- * Describes the payload type used on HTTP requests and responses when communicating RPC request and response
- * structures. Aliased here for easy refactoring.
- */
-typealias RawRpcPayload = ByteArray

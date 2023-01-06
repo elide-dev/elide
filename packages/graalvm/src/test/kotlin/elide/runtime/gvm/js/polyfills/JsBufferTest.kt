13fa2421e0aaaac2c21bfb7f7cbffb304c260782
Sam Gammon <samuel.gammon@gmail.com>
2023-01-06 14:15:03 -0800

    test: enable textencoder/textdecoder presence test

--- packages/graalvm/src/test/kotlin/elide/runtime/gvm/js/polyfills/JsBufferTest.kt
+++ packages/graalvm/src/test/kotlin/elide/runtime/gvm/js/polyfills/JsBufferTest.kt
@@ -5,7 +5,6 @@ package elide.runtime.gvm.js.polyfills
 import elide.runtime.gvm.js.AbstractJsTest
 import elide.testing.annotations.Test
 import elide.testing.annotations.TestCase
-import org.junit.jupiter.api.Disabled
 
 /** Tests that the `Buffer` polyfill is available globally. */
 @TestCase internal class JsBufferTest : AbstractJsTest() {
@@ -18,7 +17,7 @@ import org.junit.jupiter.api.Disabled
   }.doesNotFail()
 
   // `TextEncoder` type should be present globally.
-  @Test @Disabled fun testTextEncoderPresent() = executeGuest {
+  @Test fun testTextEncoderPresent() = executeGuest {
     // language=javascript
     """
       test(TextEncoder).isNotNull();
@@ -26,7 +25,7 @@ import org.junit.jupiter.api.Disabled
   }.doesNotFail()
 
   // `TextDecoder` type should be present globally.
-  @Test @Disabled fun testTextDecoderPresent() = executeGuest {
+  @Test fun testTextDecoderPresent() = executeGuest {
     // language=javascript
     """
       test(TextDecoder).isNotNull();

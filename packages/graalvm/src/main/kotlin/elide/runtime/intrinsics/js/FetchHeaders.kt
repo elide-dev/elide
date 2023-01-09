8568a2dcf09a0eab898612826a8245ab48ad3be4
Sam Gammon <samuel.gammon@gmail.com>
2023-01-09 01:22:58 -0800

    feat: Fetch API spec interfaces doc

--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/FetchHeaders.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/FetchHeaders.kt
@@ -1,43 +1,136 @@
 package elide.runtime.intrinsics.js
 
-import elide.annotations.core.Polyglot
+import elide.runtime.gvm.internals.intrinsics.js.fetch.FetchHeadersIntrinsic
+import elide.runtime.gvm.internals.intrinsics.js.struct.map.JsMultiMap
 
 /**
- * TBD.
+ * # Fetch: Headers.
+ *
+ * Describes the interface for a Fetch API compliant structure containing HTTP headers; this includes both the header
+ * names (as keys), and the header values, including multiple values per header, as applicable. Effectively, the fetch
+ * headers object acts as a container (as a [JsMultiMap], specifically of all strings).
+ *
+ * &nbsp;
+ *
+ * ## Mutability
+ *
+ * Headers can be expressed in mutable or immutable form. For example, when headers are used as inputs for a request
+ * (meaning the values originate from another machine), they are expressed in immutable form. To mutate such headers,
+ * the developer can simply construct a mutable copy using one of the constructors available via [FetchHeaders.Factory].
+ *
+ * &nbsp;
+ *
+ * ## Rendering headers
+ *
+ * Multiple header values, by spec, can often be reduced to comma-separated values within the same header name. Child
+ * implementations are expected to provide a render function which performs this task from held values.
  */
-public interface FetchHeaders {
-  /**
-   * TBD.
-   */
-  @Polyglot public fun entries(): JsIterator<Array<String>>
+public interface FetchHeaders : MultiMapLike<String, String> {
 
   /**
-   * TBD.
+   * ## Fetch: Headers factory.
+   *
+   * Specifies constructors which are expected to be made available for a given [FetchHeaders] implementation. These
+   * constructors include JavaScript-spec constructors as well as regular host-side `wrap` or `create` constructors. In
+   * all cases, these constructors are expected to return a copy of the provided data, as applicable.
    */
-  @Polyglot public fun forEach(op: (value: String, name: String) -> Unit)
+  public interface Factory<Impl> where Impl: FetchHeaders {
+    /**
+     * Create an empty set of immutable fetch headers.
+     *
+     * @return Immutable and empty set of fetch headers.
+     */
+    public fun empty(): Impl
+
+    /**
+     * Create a set of immutable fetch headers from the provided set of [pairs].
+     *
+     * @return Immutable set of fetch headers.
+     */
+    public fun fromPairs(pairs: Collection<Pair<String, String>>): Impl
+
+    /**
+     * Create a set of immutable fetch headers from the provided set of [pairs].
+     *
+     * @return Immutable set of fetch headers.
+     */
+    public fun from(vararg pairs: Pair<String, String>): Impl
+
+    /**
+     * Create a set of immutable fetch headers from the provided [map] data.
+     *
+     * @return Immutable set of fetch headers.
+     */
+    public fun fromMap(map: Map<String, String>): Impl
+
+    /**
+     * Create a set of immutable fetch headers from the provided [map] data.
+     *
+     * @return Immutable set of fetch headers.
+     */
+    public fun fromMultiMap(map: Map<String, List<String>>): Impl
+
+    /**
+     * Create an immutable copy of the provided [previous] fetch headers.
+     *
+     * @return Immutable copy of the provided fetch headers.
+     */
+    public fun from(previous: FetchHeaders): Impl
+  }
+
+  /** Factory for default-implementation instances of [FetchHeaders]. */
+  public companion object DefaultFactory : Factory<FetchHeaders> {
+    /**
+     * Create an empty set of immutable fetch headers.
+     *
+     * @return Immutable and empty set of fetch headers.
+     */
+    override fun empty(): FetchHeaders = FetchHeadersIntrinsic.empty()
+
+    /**
+     * Create a set of immutable fetch headers from the provided set of [pairs].
+     *
+     * @return Immutable set of fetch headers.
+     */
+    override fun fromPairs(pairs: Collection<Pair<String, String>>): FetchHeaders =
+      FetchHeadersIntrinsic.fromPairs(pairs)
+
+    /**
+     * Create a set of immutable fetch headers from the provided set of [pairs].
+     *
+     * @return Immutable set of fetch headers.
+     */
+    override fun from(vararg pairs: Pair<String, String>): FetchHeaders =
+      FetchHeadersIntrinsic.from(*pairs)
+
+    /**
+     * Create a set of immutable fetch headers from the provided [map] data.
+     *
+     * @return Immutable set of fetch headers.
+     */
+    override fun fromMap(map: Map<String, String>): FetchHeaders = FetchHeadersIntrinsic.fromMap(map)
+
+    /**
+     * Create a set of immutable fetch headers from the provided [map] data.
+     *
+     * @return Immutable set of fetch headers.
+     */
+    override fun fromMultiMap(map: Map<String, List<String>>): FetchHeaders = FetchHeadersIntrinsic.fromMultiMap(map)
+
+    /**
+     * Create an immutable copy of the provided [previous] fetch headers.
+     *
+     * @return Immutable copy of the provided fetch headers.
+     */
+    override fun from(previous: FetchHeaders): FetchHeaders = FetchHeadersIntrinsic.from(previous)
+  }
 
   /**
-   * TBD.
+   * Render these fetch headers into a raw map where the keys are normalized HTTP header names, and the values are
+   * either rendered into a single comma-separated string, or expressed literally, depending on the value of [flatten].
+   *
+   * @param flatten Whether to flatten repeated values into a single comma-separated value.
+   * @return Raw map as requested.
    */
-  @Polyglot public fun forEach(op: (value: String, name: String, obj: FetchHeaders) -> Unit)
-
-  /**
-   * TBD.
-   */
-  @Polyglot public fun get(name: String): String?
-
-  /**
-   * TBD.
-   */
-  @Polyglot public fun has(name: String): Boolean
-
-  /**
-   * TBD.
-   */
-  @Polyglot public fun keys(): JsIterator<String>
-
-  /**
-   * TBD.
-   */
-  @Polyglot public fun values(): JsIterator<String>
+  public fun render(flatten: Boolean = true): Map<String, List<String>>
 }
--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/ReadableStream.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/ReadableStream.kt
@@ -1,8 +1,78 @@
 package elide.runtime.intrinsics.js
 
+import elide.runtime.gvm.internals.intrinsics.js.webstreams.ReadableStreamIntrinsic
+import java.io.InputStream
+import java.io.Reader
+import java.nio.ByteBuffer
+
 /**
+ * # Web Streams: Readable Stream.
  *
+ * Models the `ReadableStream` interface, defined by the WhatWG Streams Standard (https://streams.spec.whatwg.org/).
+ * Readable streams implement streams of arbitrary data which can be consumed by an interested developer, and form part
+ * of the wider Web Streams API.
  */
 public interface ReadableStream {
-  //
+  /**
+   * ## Readable Stream: Factory.
+   *
+   * Describes constructors available both in guest and host contexts, which create [ReadableStream] implementation
+   * instances. Generally, each implementation has a factory implementation as well, from which instances can be
+   * acquired by host-side code, and, where supported, by guest-side code.
+   *
+   * @param Impl Implementation of [ReadableStream] which is created by this factory.
+   */
+  public interface Factory<Impl> where Impl: ReadableStream {
+    /**
+     * TBD.
+     */
+    public fun empty(): Impl
+
+    /**
+     * TBD.
+     */
+    public fun wrap(input: InputStream): Impl
+
+    /**
+     * TBD.
+     */
+    public fun wrap(reader: Reader): Impl
+
+    /**
+     * TBD.
+     */
+    public fun wrap(bytes: ByteArray): Impl
+
+    /**
+     * TBD.
+     */
+    public fun wrap(buffer: ByteBuffer): Impl
+  }
+
+  public companion object DefaultFactory : Factory<ReadableStream> {
+    /**
+     * TBD.
+     */
+    override fun empty(): ReadableStream = ReadableStreamIntrinsic.empty()
+
+    /**
+     * TBD.
+     */
+    override fun wrap(input: InputStream): ReadableStream = ReadableStreamIntrinsic.wrap(input)
+
+    /**
+     * TBD.
+     */
+    override fun wrap(reader: Reader): ReadableStream = ReadableStreamIntrinsic.wrap(reader)
+
+    /**
+     * TBD.
+     */
+    override fun wrap(bytes: ByteArray): ReadableStream = ReadableStreamIntrinsic.wrap(bytes)
+
+    /**
+     * TBD.
+     */
+    override fun wrap(buffer: ByteBuffer): ReadableStream = ReadableStreamIntrinsic.wrap(buffer)
+  }
 }

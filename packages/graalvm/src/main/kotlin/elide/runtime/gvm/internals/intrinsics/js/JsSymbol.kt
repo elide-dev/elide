cda5f6906f13376da8cc979e4f974350d482667a
Sam Gammon <samuel.gammon@gmail.com>
2023-01-09 01:24:57 -0800

    chore: cleanup for JS symbol/JS symbols

--- packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/intrinsics/js/JsSymbol.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/intrinsics/js/JsSymbol.kt
@@ -13,20 +13,6 @@ import kotlinx.serialization.Serializable
   java.io.Serializable {
   /** Well-known JavaScript symbols. */
   internal companion object JsSymbols {
-    // JavaScript symbol: Iterator.
-    private const val SYMBOL_ITERATOR = "iterator"
-
-    /**
-     * TBD.
-     */
-    @JvmStatic val iterator: JsSymbol = JsSymbol(SYMBOL_ITERATOR)
-
-    /** @return [JsSymbol] corresponding to the provided [symbol] string, or `null` if one cannot be resolved. */
-    fun match(symbol: String): JsSymbol? = when (symbol) {
-      SYMBOL_ITERATOR -> iterator
-      else -> null
-    }
-
     /** @return JS symbol wrapping the receiving string. */
     @JvmStatic fun String.asJsSymbol(): JsSymbol = JsSymbol(this)
   }

60b7a3a5a2e20fdcb95a0b3d3e72d202f9b608d3
Sam Gammon <samuel.gammon@gmail.com>
2023-01-13 14:51:44 -0800

    fix: doc/methods for SSR ancillary classes

--- packages/ssr/src/commonMain/kotlin/elide/runtime/ssr/CssChunk.kt
+++ packages/ssr/src/commonMain/kotlin/elide/runtime/ssr/CssChunk.kt
@@ -3,7 +3,14 @@ package elide.runtime.ssr
 import elide.annotations.core.Polyglot
 
 /**
- * TBD
+ * # SSR: CSS Chunk
+ *
+ * Data class which holds a chunk of CSS code, which should be emitted as part of a larger SSR run. This class is used
+ * in particular for integration with Emotion's SSR engine.
+ *
+ * @param ids CSS IDs held by this chunk.
+ * @param key Unique key identifying this chunk.
+ * @param css CSS code held by this chunk.
  */
 public data class CssChunk @Polyglot constructor (
   @field:Polyglot public val ids: Array<String>,
@@ -23,5 +30,9 @@ public data class CssChunk @Polyglot constructor (
 
   override fun hashCode(): Int {
     return key.hashCode()
+  }
+
+  override fun toString(): String {
+    return "CssChunk(key=$key)"
   }
 }

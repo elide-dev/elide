b1a51073c092ad75e658bd03a75cd8004522e9d3
Sam Gammon <samuel.gammon@gmail.com>
2023-01-13 13:21:51 -0800

    chore: cleanup/doc for proto gen

--- packages/model/src/commonMain/kotlin/kotlinx/serialization/protobuf/ProtoUnknown.kt
+++ packages/model/src/commonMain/kotlin/kotlinx/serialization/protobuf/ProtoUnknown.kt
@@ -6,7 +6,10 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialInfo
 
 /**
+ * # Proto: Unknown Value
  *
+ * Marks a value on a protocol-buffers enumeration as the "unknown," or default value, for the enumeration. If no value
+ * is specified over the wire, this value will be returned. The ID for this value in the enumeration is always `0`.
  */
 @SerialInfo
 @MustBeDocumented
--- packages/model/src/jvmMain/kotlin/kotlinx/serialization/protobuf/schema/ProtoBufGeneratorOptions.kt
+++ packages/model/src/jvmMain/kotlin/kotlinx/serialization/protobuf/schema/ProtoBufGeneratorOptions.kt
@@ -3,7 +3,16 @@
 package kotlinx.serialization.protobuf.schema
 
 /**
+ * # Protobuf Generator: Options
  *
+ * Specifies the set of options which apply to the protocol buffer source generator. The generator can generate Proto2
+ * or Proto3 syntax from regular Kotlin models, with special annotations for controlling the output.
+ *
+ * @param packageName package name to use for generated models; if `null`, a sensible one will be generated.
+ * @param packageOptions top-level package options to apply.
+ * @param protoOptions proto options to apply.
+ * @param emitWarningComments whether to emit file warnings indicating generated code.
+ * @param syntaxVersion protocol buffers syntax version to emit.
  */
 public data class ProtoBufGeneratorOptions(
   val packageName: String? = null,
@@ -14,7 +23,7 @@ public data class ProtoBufGeneratorOptions(
 ) {
   public companion object {
     /**
-     *
+     * Default protocol buffer generator options.
      */
     public val DEFAULTS: ProtoBufGeneratorOptions = ProtoBufGeneratorOptions()
   }

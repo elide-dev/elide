f064bfc80eb9bca6246fb244714da1f19b1e2c56
Sam Gammon <sam@elide.ventures>
2023-05-01 16:01:18 -0700

    chore: upgrade toolchain management for jvm

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ pluginManagement {
 
 plugins {
   id("com.gradle.enterprise") version("3.11.4")
-  id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
+  id("org.gradle.toolchains.foojay-resolver-convention") version("0.5.0")
 }
 
 // Fix: Force CWD to proper value and store secondary value.

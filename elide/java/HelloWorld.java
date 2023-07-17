dae5e7c901754c0c4a23804b5f29be5935a72e21
Sam Gammon <sam@elide.ventures>
2023-07-17 00:41:57 -0700

    continued structure

---
+++ elide/java/HelloWorld.java
@@ -0,0 +1,8 @@
+package elide.java;
+
+/** Sample Java file for toolchain testing. */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}

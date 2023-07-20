71b150645b5101784e6420ddedfce58d21b348d9
Sam Gammon <sam@elide.ventures>
2023-07-19 17:36:27 -0700

    fix: build cache for build cache plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 plugins {
   id("build.less") version("1.0.0-beta1")
   id("com.gradle.enterprise") version("3.13.4")
-  id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
+//  id("org.gradle.toolchains.foojay-resolver-convention") version("0.6.0")
 }
 
 // Fix: Force CWD to proper value and store secondary value.

7fb0a6d08f2b355edabdb5a55ac9350e16f29b8e
Dario Valdespino <dvaldespino00@gmail.com>
2023-07-27 16:21:37 -0400

    feat: support additional methods for express handlers.
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/intrinsics/js/express/ExpressAppIntrinsic.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/intrinsics/js/express/ExpressAppIntrinsic.kt
@@ -3,8 +3,13 @@ package elide.runtime.gvm.internals.intrinsics.js.express
 import elide.annotations.core.Polyglot
 import elide.runtime.Logging
 import elide.runtime.intrinsics.js.express.ExpressApp
+import io.netty.handler.codec.http.HttpMethod
 import org.graalvm.polyglot.Value
+import org.reactivestreams.Publisher
 import reactor.netty.http.server.HttpServer
+import reactor.netty.http.server.HttpServerRequest
+import reactor.netty.http.server.HttpServerResponse
+import reactor.netty.http.server.HttpServerRoutes
 
 /**
  * An [ExpressApp] implemented as a wrapper around a Reactor Netty server.
@@ -15,6 +20,7 @@ import reactor.netty.http.server.HttpServer
 internal class ExpressAppIntrinsic(private val context: ExpressContext) : ExpressApp {
   /** Represents a route handler registered by a guest script. */
   private data class RouteHandler(
+    val method: HttpMethod,
     val path: String,
     val handler: Value,
   )
@@ -24,22 +30,24 @@ internal class ExpressAppIntrinsic(private val context: ExpressContext) : Expres
   /** Internal collection of route handlers requested by guest scripts. */
   private val routeRegistry = mutableSetOf<RouteHandler>()
 
-  @Polyglot override fun get(path: String, handler: Value) {
-    // register the route for later use
-    routeRegistry.add(RouteHandler(path, handler))
-  }
+  @Polyglot override fun get(path: String, handler: Value) = registerRouteHandler(HttpMethod.GET, path, handler)
+  @Polyglot override fun post(path: String, handler: Value) = registerRouteHandler(HttpMethod.POST, path, handler)
+  @Polyglot override fun put(path: String, handler: Value) = registerRouteHandler(HttpMethod.PUT, path, handler)
+  @Polyglot override fun delete(path: String, handler: Value) = registerRouteHandler(HttpMethod.DELETE, path, handler)
+  @Polyglot override fun head(path: String, handler: Value) = registerRouteHandler(HttpMethod.HEAD, path, handler)
+  @Polyglot override fun options(path: String, handler: Value) = registerRouteHandler(HttpMethod.OPTIONS, path, handler)
 
   @Polyglot override fun listen(port: Int, callback: Value?) {
     // configure all the route handlers, set the port and bind the socket
     HttpServer.create().route { routes ->
-      for(route in routeRegistry) routes.get(mapExpressToReactorRoute(route.path)) { req, res ->
+      for(route in routeRegistry) route.install(routes) { handler, req, res ->
         // construct the wrappers
         val request = ExpressRequestIntrinsic(req)
         val response = ExpressResponseIntrinsic(res)
 
         // invoke the JS handler
         logging.info("Calling guest handler")
-        useCallback(route.handler) { executeVoid(request, response) }
+        useCallback(handler) { executeVoid(request, response) }
         logging.info("Exited guest handler")
 
         // return the internal publisher handled by the wrapper
@@ -52,6 +60,26 @@ internal class ExpressAppIntrinsic(private val context: ExpressContext) : Expres
 
     // notify listeners
     useCallback(callback) { executeVoid() }
+  }
+
+  private fun registerRouteHandler( method: HttpMethod, path: String, handler: Value) {
+    routeRegistry.add(RouteHandler(method, mapExpressToReactorRoute(path), handler))
+  }
+
+  private inline fun RouteHandler.install(
+    routes: HttpServerRoutes,
+    crossinline wrapper: (Value, HttpServerRequest, HttpServerResponse) -> Publisher<Void>
+  ) {
+    // Reactor Netty does not have a generic route registration method, so we need to select the function manually
+    when(method) {
+      HttpMethod.GET -> routes.get(path) { req, res -> wrapper(handler, req, res) }
+      HttpMethod.POST -> routes.post(path) { req, res -> wrapper(handler, req, res) }
+      HttpMethod.PUT -> routes.put(path) { req, res -> wrapper(handler, req, res) }
+      HttpMethod.DELETE -> routes.delete(path) { req, res -> wrapper(handler, req, res) }
+      HttpMethod.HEAD -> routes.head(path) { req, res -> wrapper(handler, req, res) }
+      HttpMethod.OPTIONS -> routes.options(path) { req, res -> wrapper(handler, req, res) }
+      else -> error("Unsupported method $method")
+    }
   }
 
   /** Safely enter the execution context and invoke the [block] on the [callback]. */

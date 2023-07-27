3bd0114b65a9302d2e59c7c3b0eea27cf2215c62
Dario Valdespino <dvaldespino00@gmail.com>
2023-07-27 16:21:37 -0400

    feat: support additional methods for express handlers.
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/express/ExpressApp.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/express/ExpressApp.kt
@@ -11,6 +11,36 @@ public interface ExpressApp {
    */
   @Polyglot public fun get(path: String, handler: Value)
   
+  /**
+   * Register a POST route [handler] at [path]. The [handler] must be a function and will receive two parameters
+   * ([ExpressRequest] and [ExpressResponse], respectively).
+   */
+  @Polyglot public fun post(path: String, handler: Value)
+
+  /**
+   * Register a PUT route [handler] at [path]. The [handler] must be a function and will receive two parameters
+   * ([ExpressRequest] and [ExpressResponse], respectively).
+   */
+  @Polyglot public fun put(path: String, handler: Value)
+
+  /**
+   * Register a DELETE route [handler] at [path]. The [handler] must be a function and will receive two parameters
+   * ([ExpressRequest] and [ExpressResponse], respectively).
+   */
+  @Polyglot public fun delete(path: String, handler: Value)
+
+  /**
+   * Register a HEAD route [handler] at [path]. The [handler] must be a function and will receive two parameters
+   * ([ExpressRequest] and [ExpressResponse], respectively).
+   */
+  @Polyglot public fun head(path: String, handler: Value)
+
+  /**
+   * Register an OPTIONS route [handler] at [path]. The [handler] must be a function and will receive two parameters
+   * ([ExpressRequest] and [ExpressResponse], respectively).
+   */
+  @Polyglot public fun options(path: String, handler: Value)
+
   /** Bind the server to a given [port]. */
   @Polyglot public fun listen(port: Int) {
     listen(port, null)

2bff935e44bc929fd076910999d400b56a4f592a
Sam Gammon <sam@elide.ventures>
2023-07-28 13:44:28 -0700

    fix: use mosaic deps from elide github packages
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -5,7 +5,7 @@ dependencyResolutionManagement {
     maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven {
-      url = uri("https://maven.pkg.github.com/sgammon/mosaic")
+      url = uri("https://maven.pkg.github.com/elide-dev/elide")
       credentials {
         username = System.getenv("GITHUB_ACTOR")
         password = System.getenv("GITHUB_TOKEN")

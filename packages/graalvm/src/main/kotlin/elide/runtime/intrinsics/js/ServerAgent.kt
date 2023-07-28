6968a2c319522bfc1b69b84903c6cf56533dc09b
Sam Gammon <sam@elide.ventures>
2023-07-27 20:22:42 -0700

    chore: refactor and introduce `ServerAgent` generic intrinsic interface
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/ServerAgent.kt
@@ -0,0 +1,24 @@
+package elide.runtime.intrinsics.js
+
+import org.graalvm.polyglot.Context as VMContext
+import java.util.concurrent.Phaser
+
+/**
+ * # Server Agent
+ *
+ * Represents an abstract intrinsic which can be used to implement a server agent. This is a special type of intrinsic
+ * which is recognized by the Elide CLI/runtime as a server; behavior is triggered which prevents an immediate exit
+ * after executing user code.
+ */
+public interface ServerAgent {
+  /**
+   * ## Server Initialization
+   *
+   * Initialize this intrinsic by passing a handle that can later be used to acquire a polyglot context. This is an
+   * internal method and should only be used by the JVM entry point.
+   *
+   * @param contextHandle Handle to the active polyglot context.
+   * @param phaserHandle Handle to the phaser used to synchronize the server.
+   */
+  public fun initialize(contextHandle: VMContext, phaserHandle: Phaser)
+}
--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/express/Express.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/express/Express.kt
@@ -1,15 +1,9 @@
 package elide.runtime.intrinsics.js.express
 
-import elide.annotations.core.Polyglot
+import elide.runtime.intrinsics.js.ServerAgent
 
 /**
  * Interface used as the root `express` module, which exports a function as default value. Invoking the function
  * returns a new [ExpressApp].
  */
-public interface Express {
-  /**
-   * Initialize this intrinsic by passing a handle that can later be used to acquire a polyglot context. This is an
-   * internal method and should only be used by the JVM entry point.
-   */
-  public fun initialize(contextHandle: Any, phaserHandle: Any)
-}
+public interface Express : ServerAgent

fee08009aa137edda413b8dc510f19585a245a2a
Sam Gammon <sam@elide.ventures>
2023-07-29 20:35:46 -0700

    chore: switch to `multiplatform` plugin for js builds
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm-js/build.gradle.kts
+++ packages/graalvm-js/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress(
   "UnstableApiUsage",
   "unused",
+  "UNUSED_VARIABLE",
   "DSL_SCOPE_VIOLATION",
 )
 
@@ -13,28 +14,36 @@ version = rootProject.version as String
 
 kotlin {
   explicitApi()
-}
 
-dependencies {
-  api(project(":packages:ssr"))
-  api(npm("esbuild", libs.versions.npm.esbuild.get()))
-  api(npm("prepack", libs.versions.npm.prepack.get()))
-  api(npm("buffer", libs.versions.npm.buffer.get()))
-  api(npm("readable-stream", libs.versions.npm.stream.get()))
-  api(npm("web-streams-polyfill", libs.versions.npm.webstreams.get()))
-  api(npm("@emotion/css", libs.versions.npm.emotion.core.get()))
-  api(npm("@emotion/server", libs.versions.npm.emotion.server.get()))
+  sourceSets {
+    val jsMain by getting {
+      dependencies {
+        api(project(":packages:ssr"))
+        api(npm("esbuild", libs.versions.npm.esbuild.get()))
+        api(npm("prepack", libs.versions.npm.prepack.get()))
+        api(npm("buffer", libs.versions.npm.buffer.get()))
+        api(npm("readable-stream", libs.versions.npm.stream.get()))
+        api(npm("web-streams-polyfill", libs.versions.npm.webstreams.get()))
+        api(npm("@emotion/css", libs.versions.npm.emotion.core.get()))
+        api(npm("@emotion/server", libs.versions.npm.emotion.server.get()))
 
-  implementation(libs.kotlinx.coroutines.core)
-  implementation(libs.kotlinx.wrappers.node)
-  implementation(libs.kotlinx.wrappers.emotion)
-  implementation(libs.kotlinx.wrappers.history)
-  implementation(libs.kotlinx.wrappers.typescript)
-  implementation(libs.kotlinx.wrappers.react.router.dom)
-  implementation(libs.kotlinx.wrappers.remix.run.router)
+        implementation(libs.kotlinx.coroutines.core)
+        implementation(libs.kotlinx.wrappers.node)
+        implementation(libs.kotlinx.wrappers.emotion)
+        implementation(libs.kotlinx.wrappers.history)
+        implementation(libs.kotlinx.wrappers.typescript)
+        implementation(libs.kotlinx.wrappers.react.router.dom)
+        implementation(libs.kotlinx.wrappers.remix.run.router)
+      }
+    }
 
-  // Testing
-  testImplementation(project(":packages:test"))
+    val jsTest by getting {
+      dependencies {
+        // Testing
+        implementation(project(":packages:test"))
+      }
+    }
+  }
 }
 
 val buildDocs = project.properties["buildDocs"] == "true"

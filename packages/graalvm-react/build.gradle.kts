ef888008cecc41f1f05d9b779ac2443e97c3d4ad
Sam Gammon <sam@elide.ventures>
2023-07-30 01:39:04 -0700

    chore: convert `graalvm-react` into multiplatform module
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm-react/build.gradle.kts
+++ packages/graalvm-react/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress(
   "UnstableApiUsage",
   "unused",
+  "UNUSED_VARIABLE",
   "DSL_SCOPE_VIOLATION",
 )
 
@@ -11,31 +12,40 @@ plugins {
 group = "dev.elide"
 version = rootProject.version as String
 
-dependencies {
-  api(npm("esbuild", libs.versions.npm.esbuild.get()))
-  api(npm("prepack", libs.versions.npm.prepack.get()))
-  api(npm("buffer", libs.versions.npm.buffer.get()))
-  api(npm("readable-stream", libs.versions.npm.stream.get()))
+kotlin {
+  sourceSets {
+    val jsMain by getting {
+      dependencies {
+        api(npm("esbuild", libs.versions.npm.esbuild.get()))
+        api(npm("prepack", libs.versions.npm.prepack.get()))
+        api(npm("buffer", libs.versions.npm.buffer.get()))
+        api(npm("readable-stream", libs.versions.npm.stream.get()))
 
-  implementation(project(":packages:graalvm-js"))
+        implementation(project(":packages:graalvm-js"))
 
-  implementation(libs.kotlinx.wrappers.node)
-  implementation(libs.kotlinx.wrappers.mui)
-  implementation(libs.kotlinx.wrappers.react)
-  implementation(libs.kotlinx.wrappers.react.dom)
-  implementation(libs.kotlinx.wrappers.react.router.dom)
-  implementation(libs.kotlinx.wrappers.remix.run.router)
-  implementation(libs.kotlinx.coroutines.core.js)
-  implementation(libs.kotlinx.serialization.core.js)
-  implementation(libs.kotlinx.serialization.json.js)
-  implementation(libs.kotlinx.wrappers.css)
-  implementation(libs.kotlinx.wrappers.emotion)
-  implementation(libs.kotlinx.wrappers.browser)
-  implementation(libs.kotlinx.wrappers.history)
-  implementation(libs.kotlinx.wrappers.typescript)
+        implementation(libs.kotlinx.wrappers.node)
+        implementation(libs.kotlinx.wrappers.mui)
+        implementation(libs.kotlinx.wrappers.react)
+        implementation(libs.kotlinx.wrappers.react.dom)
+        implementation(libs.kotlinx.wrappers.react.router.dom)
+        implementation(libs.kotlinx.wrappers.remix.run.router)
+        implementation(libs.kotlinx.coroutines.core.js)
+        implementation(libs.kotlinx.serialization.core.js)
+        implementation(libs.kotlinx.serialization.json.js)
+        implementation(libs.kotlinx.wrappers.css)
+        implementation(libs.kotlinx.wrappers.emotion)
+        implementation(libs.kotlinx.wrappers.browser)
+        implementation(libs.kotlinx.wrappers.history)
+        implementation(libs.kotlinx.wrappers.typescript)
+      }
+    }
 
-  // Testing
-  testImplementation(project(":packages:test"))
+    val jsTest by getting {
+      dependencies {
+        implementation(project(":packages:test"))
+      }
+    }
+  }
 }
 
 val buildDocs = project.properties["buildDocs"] == "true"

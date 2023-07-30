d25ae250b70e9d92f24ce9183c69e47073961d57
Sam Gammon <sam@elide.ventures>
2023-07-30 14:41:51 -0700

    chore: general build cleanup for push
    Signed-off-by: Sam Gammon <sam@elide.dev>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/cli/src/main/kotlin/module-info.java
+++ packages/cli/src/main/kotlin/module-info.java
@@ -6,19 +6,28 @@ module elide.cli {
     requires kotlin.stdlib;
     requires kotlinx.coroutines.core;
 
+    requires io.micronaut.inject;
+    requires io.micronaut.picocli.picocli;
+
     requires org.graalvm.sdk;
     requires org.graalvm.truffle;
 
     requires ch.qos.logback.classic;
     requires info.picocli;
+    requires info.picocli.jansi.graalvm;
     requires org.slf4j;
     requires org.jline.console;
     requires org.jline.style;
     requires org.jline.terminal;
+    requires jul.to.slf4j;
 
     requires elide.core;
     requires elide.base;
     requires elide.graalvm;
+    requires org.jline.reader;
+    requires ch.qos.logback.core;
+    requires io.micronaut.core;
+    requires org.jline.builtins;
 
     exports elide.tool.cli;
     exports elide.tool.cli.cfg;

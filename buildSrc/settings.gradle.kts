7f9b4388ae1e54b9534e7b43f5db975558c2f069
Sam Gammon <sam@elide.ventures>
2023-07-30 17:50:08 -0700

    fix: use wasm repositories codebase wide
    Signed-off-by: Sam Gammon <sam@elide.dev>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -3,6 +3,7 @@ dependencyResolutionManagement {
     maven("https://maven.pkg.st/")
     maven("https://gradle.pkg.st/")
     maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
   }
   versionCatalogs {
     create("libs") {
@@ -16,6 +17,7 @@ pluginManagement {
     maven("https://gradle.pkg.st/")
     maven("https://maven.pkg.st/")
     maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
+    maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
   }
 }
 

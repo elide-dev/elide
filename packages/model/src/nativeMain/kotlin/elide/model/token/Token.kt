be903eff7c1e207a229154311e129ce192efc5ba
Sam Gammon <sam@elide.ventures>
2023-08-01 06:13:56 -0700

    fix: missing imports in `Token` and `TokenValue` models

--- packages/model/src/nativeMain/kotlin/elide/model/token/Token.kt
+++ packages/model/src/nativeMain/kotlin/elide/model/token/Token.kt
@@ -1,6 +1,7 @@
 package elide.model.token
 
 import elide.annotations.data.Sensitive
+import kotlinx.serialization.Serializable
 
 /** Describes a sensitive token value. */
 @Serializable public actual data class Token (

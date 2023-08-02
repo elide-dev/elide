32e1d0abd059421fd72a1dcca097565521ff3474
Sam Gammon <sam@elide.ventures>
2023-08-01 19:15:28 -0700

    fix: wasm build stash
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/wasm/src/wasmMain/kotlin/elide/wasm/WasiClocks.kt
+++ packages/wasm/src/wasmMain/kotlin/elide/wasm/WasiClocks.kt
@@ -13,9 +13,10 @@
 
 package elide.wasm
 
-import org.kowasm.wasi.internal.ClockId
-import org.kowasm.wasi.internal.clockResGet
-import org.kowasm.wasi.internal.clockTimeGet
+import elide.wasm.internal.ClockId
+import elide.wasm.internal.clockResGet
+import elide.wasm.internal.clockTimeGet
+//import kotlinx.coroutines.Deferred
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
@@ -25,7 +26,10 @@ import kotlin.time.toDuration
 typealias Instant = Long
 
 /** A time and date in seconds plus nanoseconds. */
-data class DateTime(val seconds: Long, val nanoseconds: Int)
+data class DateTime(
+  val seconds: Long,
+  val nanoseconds: Int,
+)
 
 /**
  * A monotonic clock is a clock which has an unspecified initial value, and
@@ -34,22 +38,20 @@ data class DateTime(val seconds: Long, val nanoseconds: Int)
  * It is intended for measuring elapsed time.
  */
 interface MonotonicClock {
+  /** Read the current value of the clock.
+   *
+   * As this the clock is monotonic, calling this function repeatedly will produce
+   * a sequence of non-decreasing values.
+   */
+  fun now(): Instant
 
-    /** Read the current value of the clock.
-     *
-     * As this the clock is monotonic, calling this function repeatedly will produce
-     * a sequence of non-decreasing values.
-     */
-    fun now(): Instant
+  /** Query the resolution of the clock. **/
+  fun resolution(): Instant
 
-    /** Query the resolution of the clock. **/
-    fun resolution(): Instant
-
-    /** This creates a new [MonotonicTimer] with the given starting time. It will
-     * count down from this time until it reaches zero.
-     */
-    fun newTimer(initial: Instant): MonotonicTimer
-
+  /** This creates a new [MonotonicTimer] with the given starting time. It will
+   * count down from this time until it reaches zero.
+   */
+  fun newTimer(initial: Instant): MonotonicTimer
 }
 
 /**
@@ -62,30 +64,28 @@ interface MonotonicClock {
  * It is intended for reporting the current date and time for humans.
  */
 interface WallClock {
+  /**
+   * Read the current value of the clock.
+   *
+   * As this the clock is not monotonic, calling this function repeatedly will
+   * not necessarily produce a sequence of non-decreasing values.
+   *
+   * The returned timestamps represent the number of seconds since
+   * 1970-01-01T00:00:00Z, also known as [POSIX's Seconds Since the Epoch], also
+   * known as [Unix Time].
+   *
+   * The nanoseconds field of the output is always less than 1000000000.
+   *
+   * [POSIX's Seconds Since the Epoch]: https://pubs.opengroup.org/onlinepubs/9699919799/xrat/V4_xbd_chap04.html#tag_21_04_16
+   * [Unix Time]: https://en.wikipedia.org/wiki/Unix_time
+   */
+  fun now(): DateTime
 
-    /**
-     * Read the current value of the clock.
-     *
-     * As this the clock is not monotonic, calling this function repeatedly will
-     * not necessarily produce a sequence of non-decreasing values.
-     *
-     * The returned timestamps represent the number of seconds since
-     * 1970-01-01T00:00:00Z, also known as [POSIX's Seconds Since the Epoch], also
-     * known as [Unix Time].
-     *
-     * The nanoseconds field of the output is always less than 1000000000.
-     *
-     * [POSIX's Seconds Since the Epoch]: https://pubs.opengroup.org/onlinepubs/9699919799/xrat/V4_xbd_chap04.html#tag_21_04_16
-     * [Unix Time]: https://en.wikipedia.org/wiki/Unix_time
-     */
-    fun now(): DateTime
-
-    /**
-     * Query the resolution of the clock.
-     * The nanoseconds field of the output is always less than 1000000000.
-     */
-    fun resolution(): DateTime
-
+  /**
+   * Query the resolution of the clock.
+   * The nanoseconds field of the output is always less than 1000000000.
+   */
+  fun resolution(): DateTime
 }
 
 /**
@@ -93,13 +93,11 @@ interface WallClock {
  * on a monotonic clock.
  */
 interface MonotonicTimer {
+  /** Returns the amount of time left before this timer reaches zero. **/
+  fun current(): Instant
 
-    /** Returns the amount of time left before this timer reaches zero. **/
-    fun current(): Instant
-
-    /** Returns a future that completes when the timer reaches zero. **/
-    // fun expiration(): Deferred<Unit>
-
+  /** Returns a future that completes when the timer reaches zero. **/
+//   fun expiration(): Deferred<Unit>
 }
 
 /**
@@ -110,38 +108,34 @@ interface MonotonicTimer {
  * calling this function each time.
  */
 object DefaultMonotonicClock : MonotonicClock {
+  override fun now(): Instant {
+    val resolution = clockResGet(ClockId.MONOTONIC)
+    return clockTimeGet(ClockId.MONOTONIC, resolution)
+  }
 
-    override fun now(): Instant {
-        val resolution = clockResGet(ClockId.MONOTONIC)
-        return clockTimeGet(ClockId.MONOTONIC, resolution)
-    }
+  override fun resolution(): Instant {
+    return clockResGet(ClockId.MONOTONIC)
+  }
 
-    override fun resolution(): Instant {
-        return clockResGet(ClockId.MONOTONIC)
-    }
-
-    override fun newTimer(initial: Instant): MonotonicTimer {
-        TODO("Not yet implemented")
-    }
-
+  override fun newTimer(initial: Instant): MonotonicTimer {
+    TODO("Not yet implemented")
+  }
 }
 
 /**
  * Default wall clock, suitable for general-purpose application needs.
  */
 object DefaultWallClock : WallClock {
+  override fun now(): DateTime {
+    val resolution = clockResGet(ClockId.REALTIME)
+    val timestamp = clockTimeGet(ClockId.REALTIME, resolution)
+    val duration = timestamp.toDuration(DurationUnit.NANOSECONDS)
+    return duration.toComponents { seconds, nanoseconds -> DateTime(seconds, nanoseconds) }
+  }
 
-    override fun now(): DateTime {
-        val resolution = clockResGet(ClockId.REALTIME)
-        val timestamp = clockTimeGet(ClockId.REALTIME, resolution)
-        val duration = timestamp.toDuration(DurationUnit.NANOSECONDS)
-        return duration.toComponents { seconds, nanoseconds -> DateTime(seconds, nanoseconds) }
-    }
-
-    override fun resolution(): DateTime {
-        val resolution = clockResGet(ClockId.REALTIME)
-        val duration = resolution.toDuration(DurationUnit.NANOSECONDS)
-        return duration.toComponents { seconds, nanoseconds -> DateTime(seconds, nanoseconds) }
-    }
-
+  override fun resolution(): DateTime {
+    val resolution = clockResGet(ClockId.REALTIME)
+    val duration = resolution.toDuration(DurationUnit.NANOSECONDS)
+    return duration.toComponents { seconds, nanoseconds -> DateTime(seconds, nanoseconds) }
+  }
 }

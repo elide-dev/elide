4e107b25554a5d13a29f1e791cafcf6518e7ac05
Sam Gammon <sam@elide.ventures>
2023-08-03 16:45:23 -0700

    chore: copyright headers for benchmarks
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- benchmarks/buildSrc/build.gradle.kts
+++ benchmarks/buildSrc/build.gradle.kts
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2023 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
 
 plugins {
   id("dev.elide.build")
@@ -14,6 +26,7 @@ repositories {
 val kotlinVersion = "1.9.0"
 
 dependencies {
+  api(libs.elide.tools.conventions)
   implementation(libs.plugin.kotlin.allopen)
   implementation(libs.plugin.kotlin.noarg)
   implementation(libs.plugin.kotlinx.serialization)

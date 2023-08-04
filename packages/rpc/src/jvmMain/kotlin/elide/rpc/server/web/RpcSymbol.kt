29fcf25a89101f7ea65ec5b6ed76f11db8a7d927
Sam Gammon <sam@elide.ventures>
2023-08-03 20:54:00 -0700

    chore: license headers in `test`, `server`, `proto`, `rpc`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/rpc/src/jvmMain/kotlin/elide/rpc/server/web/RpcSymbol.kt
+++ packages/rpc/src/jvmMain/kotlin/elide/rpc/server/web/RpcSymbol.kt
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2023 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
 package elide.rpc.server.web
 
 /**
@@ -6,7 +19,7 @@ package elide.rpc.server.web
  * The [TRAILER] value is used to encode gRPC responses over regular HTTP/1.1-style responses, if needed. The [DATA]
  * symbol is used to demarcate a data frame inside a gRPC Web request or response.
  */
-public enum class RpcSymbol constructor (public val value: Byte) {
+public enum class RpcSymbol (public val value: Byte) {
   /** Symbol indicating a data frame. */
   DATA((0x00).toByte()),
 

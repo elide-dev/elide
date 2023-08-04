4f7fe22be14600bfaa2363acdf32a30597dd47dd
Sam Gammon <sam@elide.ventures>
2023-08-04 00:28:15 -0700

    chore: update uses of `Symbolic` and related resolver
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/proto/proto-kotlinx/src/jvmMain/kotlin/elide/proto/impl/data/KxEncoding.kt
+++ packages/proto/proto-kotlinx/src/jvmMain/kotlin/elide/proto/impl/data/KxEncoding.kt
@@ -15,11 +15,12 @@
 
 package elide.proto.impl.data
 
+import elide.core.api.Symbolic
+import elide.core.api.Symbolic.SealedResolver
 import elide.data.Encoding
-import elide.proto.api.Symbolic
 
 /** TBD. */
-public enum class KxEncoding constructor (override val symbol: Int) : Symbolic<Int> {
+public enum class KxEncoding (override val symbol: Int) : Symbolic<Int> {
   /** Hex encoding. */
   HEX(symbol = Encoding.HEX.ordinal),
 
@@ -27,9 +28,9 @@ public enum class KxEncoding constructor (override val symbol: Int) : Symbolic<I
   BASE64(symbol = Encoding.BASE64.ordinal);
 
   /** Resolves integer symbols to [KxEncoding] enumeration entries. */
-  public companion object : Symbolic.Resolver<Int, KxEncoding> {
+  public companion object : SealedResolver<Int, KxEncoding> {
     /** @return [KxEncoding] for the provided raw integer [symbol]. */
-    @JvmStatic override fun resoleSymbol(symbol: Int): KxEncoding = when (symbol) {
+    @JvmStatic override fun resolve(symbol: Int): KxEncoding = when (symbol) {
       Encoding.HEX.ordinal -> HEX
       Encoding.BASE64.ordinal -> BASE64
       else -> throw unresolved(symbol)

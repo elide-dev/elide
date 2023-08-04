7175ba39c92527d8d8e0d690247f3f95287a3d93
Sam Gammon <sam@elide.ventures>
2023-08-03 19:49:21 -0700

    chore: frontend dependency updates
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm-js/src/jsMain/kotlin/webstreams/polyfill/webstreams.kt
+++
@@ -1,6 +0,0 @@
-@file:JsModule("web-streams-polyfill/ponyfill")
-
-package webstreams.polyfill
-
-/** Readable stream. */
-public abstract external class ReadableStream<R>: web.streams.ReadableStream<R>

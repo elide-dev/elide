a5c00439f026a0354393714c8607a6ee44db7f4b
Sam Gammon <sam@elide.ventures>
2023-08-03 20:54:00 -0700

    chore: license headers in `test`, `server`, `proto`, `rpc`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/rpc/src/jvmMain/kotlin/elide/rpc/server/web/GrpcWebContentType.kt
+++ packages/rpc/src/jvmMain/kotlin/elide/rpc/server/web/GrpcWebContentType.kt
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2023 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
 package elide.rpc.server.web
 
 import io.micronaut.http.MediaType
@@ -7,7 +20,7 @@ import io.micronaut.http.MediaType
  *
  * @param symbol HTTP `Content-Type` value corresponding to this format.
  */
-public enum class GrpcWebContentType constructor (internal val symbol: String) {
+public enum class GrpcWebContentType (internal val symbol: String) {
   /**
    * Binary dispatch for gRPC-Web, potentially with Protocol Buffers.
    *
--- packages/test/src/jvmMain/kotlin/elide/server/TestUtil.kt
+++ packages/test/src/jvmMain/kotlin/elide/server/TestUtil.kt
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2023 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
 package elide.server
 
 /** General utilities for use in Elide tests. */
@@ -8,9 +21,9 @@ public object TestUtil {
    * @param name Name of the resource to load
    * @return File contents.
    */
-  public fun loadFixture(name: String): String = TestUtil::class.java.getResourceAsStream(
+  public fun loadFixture(name: String): String = requireNotNull(TestUtil::class.java.getResourceAsStream(
     name
-  ).bufferedReader().use {
+  )).bufferedReader().use {
     it.readText()
   }
 
@@ -20,9 +33,9 @@ public object TestUtil {
    * @param name Name of the resource to load
    * @return File contents.
    */
-  public fun loadBinary(name: String): ByteArray = TestUtil::class.java.getResourceAsStream(
+  public fun loadBinary(name: String): ByteArray = requireNotNull(TestUtil::class.java.getResourceAsStream(
     name
-  ).buffered().use {
+  )).buffered().use {
     it.readAllBytes()
   }
 }

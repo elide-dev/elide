6ec0964983095cf90462e90766484907d9a55e12
Dario <dvaldespino00@gmail.com>
2023-08-15 19:31:15 -0400

    test(core): intrinsic bindings tests.
    Signed-off-by: Dario <dvaldespino00@gmail.com>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/runtime-core/src/test/kotlin/elide/runtime/core/ContextTest.kt
+++ packages/runtime-core/src/test/kotlin/elide/runtime/core/ContextTest.kt
@@ -2,10 +2,11 @@ package elide.runtime.core
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.random.Random
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-@OptIn(DelicateElideApi::class)
-class ContextTest {
+@OptIn(DelicateElideApi::class) class ContextTest {
   /** Simple mock implementation for a JS language marker. */
   private object JavaScript : GuestLanguage {
     override val languageId: String = "js"
@@ -14,6 +15,45 @@ class ContextTest {
   /** Simple mock implementation for a Python language marker. */
   private object Python : GuestLanguage {
     override val languageId: String = "python"
+  }
+
+  @Test fun testIntrinsicBindings() {
+    val context = PolyglotEngine {
+      enableLanguage(Python)
+      enableLanguage(JavaScript)
+    }.acquire()
+
+    val (globalKey, globalValue) = "globalTestBinding" to Random.nextInt()
+    val (jsKey, jsValue) = "jsTestBinding" to Random.nextInt()
+
+    // set a global binding
+    context.bindings(language = null).putMember(globalKey, globalValue)
+
+    // set a language-scoped binding
+    context.bindings(language = JavaScript).putMember(jsKey, jsValue)
+
+    assertEquals(
+      expected = jsValue,
+      actual = context.execute(language = JavaScript, source = jsKey).asInt(),
+      message = "language-scoped binding should be accessible in target language",
+    )
+
+    assertTrue(
+      actual = context.execute(language = Python, source = jsKey).isNull,
+      message = "language-scoped binding should not be accessible to other languages",
+    )
+
+    assertEquals(
+      expected = globalValue,
+      actual = context.execute(language = JavaScript, source = globalKey).asInt(),
+      message = "global binding should be accessible in JavaScript",
+    )
+
+    assertEquals(
+      expected = globalValue,
+      actual = context.execute(language = Python, source = globalKey).asInt(),
+      message = "global binding should be accessible in Python",
+    )
   }
 
   @Test fun testEnableLanguage() {
@@ -47,7 +87,7 @@ class ContextTest {
     
     getMessage("Elide")
     """
-    
+
     assertEquals(
       expected = "Hello, Elide",
       actual = context.execute(JavaScript, code).asString(),

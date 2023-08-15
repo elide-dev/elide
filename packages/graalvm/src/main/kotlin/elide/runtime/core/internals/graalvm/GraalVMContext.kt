f618421c939c8cf8b47e6b2e445185430b814f80
Dario <dvaldespino00@gmail.com>
2023-08-15 11:36:42 -0400

    feat(core): intrinsic bindings support.
    Signed-off-by: Dario <dvaldespino00@gmail.com>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/src/main/kotlin/elide/runtime/core/internals/graalvm/GraalVMContext.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/core/internals/graalvm/GraalVMContext.kt
@@ -3,13 +3,17 @@ package elide.runtime.core.internals.graalvm
 import org.graalvm.polyglot.Context
 import elide.runtime.core.DelicateElideApi
 import elide.runtime.core.GuestLanguage
-import elide.runtime.core.PolyglotValue
 import elide.runtime.core.PolyglotContext
+import elide.runtime.core.PolyglotValue
 
 /**
  * An implementation of the [PolyglotContext] interface wrapping a GraalVM context.
  */
 @DelicateElideApi internal class GraalVMContext(val context: Context) : PolyglotContext {
+  override fun bindings(language: GuestLanguage?): PolyglotValue {
+    return language?.let { context.getBindings(it.languageId) } ?: context.polyglotBindings
+  }
+
   override fun execute(language: GuestLanguage, source: String): PolyglotValue {
     return context.eval(language.languageId, source)
   }

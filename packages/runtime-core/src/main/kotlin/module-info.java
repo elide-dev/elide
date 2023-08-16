9907f9ec46ec05a7a70e7ac89f14712c2bc3aac7
Dario <dvaldespino00@gmail.com>
2023-08-16 18:40:30 -0400

    feat(core): JavaScript plugin.
    Signed-off-by: Dario <dvaldespino00@gmail.com>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/runtime-core/src/main/kotlin/module-info.java
+++ packages/runtime-core/src/main/kotlin/module-info.java
@@ -2,6 +2,8 @@ module elide.runtime.core {
   requires kotlin.stdlib;
   requires org.graalvm.sdk;
   requires elide.graalvm;
+    requires kotlinx.serialization.core;
+  requires kotlinx.serialization.json;
 
   exports elide.runtime.core;
   exports elide.runtime.core.extensions;

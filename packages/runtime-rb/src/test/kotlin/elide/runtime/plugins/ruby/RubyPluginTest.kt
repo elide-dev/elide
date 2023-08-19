dabab6c7b3236e0906579c5057e0371c0d7d3512
Dario Valdespino <dvaldespino00@gmail.com>
2023-08-19 19:21:46 -0400

    feat(core): Ruby plugin.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ packages/runtime-rb/src/test/kotlin/elide/runtime/plugins/ruby/RubyPluginTest.kt
@@ -0,0 +1,28 @@
+package elide.runtime.plugins.ruby
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import elide.runtime.core.DelicateElideApi
+import elide.runtime.core.PolyglotEngine
+
+@OptIn(DelicateElideApi::class)
+internal class RubyPluginTest {
+  @Test fun testExecution() {
+    val engine = PolyglotEngine { install(Ruby) }
+    val context = engine.acquire()
+    
+    val result = context.ruby("""
+    def getValue()
+      return 42
+    end
+    
+    getValue()
+    """.trimIndent())
+    
+    assertEquals(
+      expected = 42,
+      actual = result.asInt(),
+      message = "should return correct value",
+    )
+  }
+}

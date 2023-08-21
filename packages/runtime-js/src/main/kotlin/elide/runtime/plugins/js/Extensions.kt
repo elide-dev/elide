097ec2efbb670a63feed67e26b0716e6bc36a6a2
Dario Valdespino <dvaldespino00@gmail.com>
2023-08-20 23:43:00 -0400

    fix(core): support ESM source execution in JS plugin.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/runtime-js/src/main/kotlin/elide/runtime/plugins/js/Extensions.kt
+++ packages/runtime-js/src/main/kotlin/elide/runtime/plugins/js/Extensions.kt
@@ -1,5 +1,6 @@
 package elide.runtime.plugins.js
 
+import org.graalvm.polyglot.Source
 import elide.runtime.core.DelicateElideApi
 import elide.runtime.core.PolyglotContext
 import elide.runtime.core.PolyglotValue
@@ -10,8 +11,18 @@ import elide.runtime.core.execute
  * [PolyglotContext.execute] and selecting [JavaScript] as source language.
  *
  * @param source The JavaScript source code to be executed.
- * @return The result of the invocation.
+ * @param esm Whether to treat the [source] as an ESM module. If false, the code is evaluted as CommonJS source.
+ * @return The result of the invocation. If [esm] is `true`, an object is returned, with exported values as members.
  */
-@DelicateElideApi public fun PolyglotContext.javaScript(/*language=javascript*/ source: String): PolyglotValue {
-  return execute(JavaScript, source)
+@DelicateElideApi public fun PolyglotContext.javaScript(
+  source: String,
+  esm: Boolean = false,
+): PolyglotValue {
+  val src = Source.newBuilder(
+    /* language = */ JavaScript.languageId,
+    /* characters = */ source,
+    /* name = */ if(esm) "source.mjs" else "source.js",
+   ).build()
+
+  return execute(src)
 }

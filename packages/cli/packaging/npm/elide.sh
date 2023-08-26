8536e1426d2a6a68d2ccbb5021bbddf2db7592fa
Sam Gammon <sam@elide.ventures>
2023-08-26 12:51:48 -0700

    chore: version bump → `1.0-v3-alpha4-b11`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/cli/packaging/npm/elide.sh
+++ packages/cli/packaging/npm/elide.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-latest_known_version="1.0-v3-alpha4-b10";
+latest_known_version="1.0-v3-alpha4-b11";
 
 # detect elide command, if it is not here, echo a message
 # and exit with a non-zero exit code

9743c6240655040f2ba33d94ed3ed96647583339
Sam Gammon <sam@elide.ventures>
2023-08-27 22:31:18 -0700

    fix: use new inlining properties for gvm context
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/context/NativeContextManagerImpl.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/context/NativeContextManagerImpl.kt
@@ -78,8 +78,8 @@ import elide.runtime.gvm.internals.VMStaticProperty as StaticProperty
       StaticProperty.active("engine.Compilation"),
       StaticProperty.active("engine.MultiTier"),
       StaticProperty.active("engine.Splitting"),
-      StaticProperty.active("compiler.Inlining"),
-      StaticProperty.active("compiler.InlineAcrossTruffleBoundary"),
+      StaticProperty.active("engine.Inlining"),
+      StaticProperty.active("engine.InlineAcrossTruffleBoundary"),
 
       // isolate options
       if (!enableIsolates) null else StaticProperty.inactive("engine.SpawnIsolate"),

6296b1d12db7607444c9477ff93a818b881b4f82
Sam Gammon <sam@elide.ventures>
2023-09-13 01:54:54 -0700

    fix: dedicated `javaLauncher` spec for run tasks
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/build.gradle.kts
+++ packages/graalvm/build.gradle.kts
@@ -229,6 +229,13 @@ tasks {
     maxParallelForks = 4
     environment("ELIDE_TEST", "true")
     systemProperty("elide.test", "true")
+
+    javaToolchains {
+      javaLauncher.set(launcherFor {
+        languageVersion = JavaLanguageVersion.of(21)
+        vendor = JvmVendorSpec.GRAAL_VM
+      })
+    }
   }
 
   /**

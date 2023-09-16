6c199906572a035a551c663668aed58ac45e195a
Dario Valdespino <dvaldespino00@gmail.com>
2023-09-16 19:28:22 -0400

    fix(build): correct multiplatform target declarations.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- tools/elide-build/src/main/kotlin/elide/internal/conventions/kotlin/KotlinTarget.kt
+++ tools/elide-build/src/main/kotlin/elide/internal/conventions/kotlin/KotlinTarget.kt
@@ -7,13 +7,17 @@ public sealed interface KotlinTarget {
   public data object JVM : KotlinTarget
   public data object WASM : KotlinTarget
 
-  @JvmInline public value class Multiplatform(
-    public val targets: Array<KotlinTarget>,
-  ) : KotlinTarget
+  @JvmInline public value class Multiplatform(public val targets: Array<KotlinTarget>) : KotlinTarget
 
-  public operator fun plus(other: KotlinTarget): Multiplatform = when (other) {
-    is Multiplatform -> Multiplatform(other.targets + this)
-    else -> Multiplatform(arrayOf(this, other))
+  public operator fun plus(other: KotlinTarget): Multiplatform = when(this){
+    is Multiplatform -> when(other) {
+      is Multiplatform -> Multiplatform(targets + other.targets)
+      else -> Multiplatform(targets + other)
+    }
+    else -> when(other) {
+      is Multiplatform -> Multiplatform(other.targets + this)
+      else -> Multiplatform(arrayOf(this, other))
+    }
   }
 
   public operator fun contains(other: KotlinTarget): Boolean = when (this) {
@@ -22,6 +26,6 @@ public sealed interface KotlinTarget {
   }
 
   public companion object {
-    public val All: KotlinTarget = Multiplatform(arrayOf(JVM, JsBrowser, JsNode, Native))
+    public val All: KotlinTarget by lazy { Multiplatform(arrayOf(JVM, JsBrowser, JsNode, Native)) }
   }
 }

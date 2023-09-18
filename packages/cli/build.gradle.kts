eb06d3adcdb741f740efa853e9e8e45b4ab5a989
Sam Gammon <sam@elide.ventures>
2023-09-17 19:53:19 -0700

    feat: enable logback xml to java opt
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/cli/build.gradle.kts
+++ packages/cli/build.gradle.kts
@@ -487,7 +487,7 @@ micronaut {
     precomputeOperations = true
     cacheEnvironment = true
     deduceEnvironment = true
-    replaceLogbackXml = false
+    replaceLogbackXml = true
 
     optimizeServiceLoading = true
     optimizeClassLoading = true

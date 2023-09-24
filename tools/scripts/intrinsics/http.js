f949f04a7bb96d5de47fa55ae95087c3390cc871
Dario Valdespino <dvaldespino00@gmail.com>
2023-09-23 23:25:21 -0400

    feat: add sample script for server intrinsics.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

---
+++ tools/scripts/intrinsics/http.js
@@ -0,0 +1,25 @@
+// retrieve the binding from global symbols
+console.log("Resolving router intrinsics");
+const router = Elide.http.router;
+
+// define local dependencies to demonstrate guest references
+// (note that this counter will be thread-local)
+const decorations = ["🚀", "👋,", "🙂", "👍", "🎉", "✨"];
+let counter = 0;
+
+// iterate over different emojis for an easy visual demo
+function getMessage() {
+  const message = `${decorations[counter % decorations.length]} Hello Elide!`;
+  counter += 1;
+
+  return message;
+}
+
+// define route handlers
+console.log("Configuring route handlers");
+router.handle("GET", "/hello", (request, response, context) => {
+  response.send(200, getMessage());
+});
+
+// we're done!
+console.log("✨ Configuration finished! ✨");

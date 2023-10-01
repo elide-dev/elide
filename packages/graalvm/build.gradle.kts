beedbfad6dfaab7940d1c9524ad5de8b4262438a
Sam Gammon <sam@elide.ventures>
2023-09-30 18:47:49 -0700

    chore: fixes for new use of capnp deps
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/build.gradle.kts
+++ packages/graalvm/build.gradle.kts
@@ -248,6 +248,7 @@ dependencies {
   implementation(projects.packages.proto.protoCore)
   implementation(projects.packages.proto.protoProtobuf)
   implementation(projects.packages.proto.protoKotlinx)
+  implementation(projects.packages.proto.protoFlatbuffers)
   implementation(projects.packages.proto.protoCapnp)
   implementation(libs.capnproto.runtime)
   implementation(libs.capnproto.runtime.rpc)

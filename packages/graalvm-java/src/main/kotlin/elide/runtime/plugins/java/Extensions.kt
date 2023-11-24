e63329c771b7b97f2c35175995d9ded6fe4fb3e7
Dario Valdespino <dvaldespino00@gmail.com>
2023-11-23 21:42:36 -0500

    feat: kotlin and java extensions for quick evaluation.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

---
+++ packages/graalvm-java/src/main/kotlin/elide/runtime/plugins/java/Extensions.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
+package elide.runtime.plugins.java
+
+
+import org.graalvm.polyglot.Source
+import elide.runtime.core.DelicateElideApi
+import elide.runtime.core.PolyglotContext
+import elide.runtime.core.PolyglotValue
+import elide.runtime.core.evaluate
+
+/**
+ * Execute the given Java [source] code as an interactive snippet, returning the result. This is equivalent to
+ * calling [PolyglotContext.evaluate] and selecting [Java] as source language with an interactive source.
+ *
+ * @param source The Java snippet to be evaluated.
+ * @return The result of the invocation.
+ */
+@DelicateElideApi public fun PolyglotContext.java(source: String, name: String? = null): PolyglotValue {
+  return evaluate(
+    Source.newBuilder(Java.languageId, source, name ?: "snippet.java")
+      .interactive(true)
+      .build(),
+  )
+}

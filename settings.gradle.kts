dd85a52091264aadd7cf557a566095478d9ac431
Sam Gammon <sam@elide.ventures>
2023-12-10 01:33:53 -0800

    chore: upgrade gradle userdata plugin
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,7 +32,7 @@ plugins {
   id("build.less") version("1.0.0-beta9")
   id("com.gradle.enterprise") version("3.15.1")
   id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
-  id("com.gradle.common-custom-user-data-gradle-plugin") version("1.11.1")
+  id("com.gradle.common-custom-user-data-gradle-plugin") version("1.12")
   id("io.micronaut.platform.catalog") version(extra.properties["micronautVersion"] as String)
 }
 

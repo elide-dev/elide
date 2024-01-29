734ccf671a763a6120c868a4d22aa64c9a0ff42e
Sam Gammon <sam@elide.ventures>
2024-01-29 01:04:03 -0800

    feat: add initial `serverless` and `embedded` packages
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ packages/embedded/build.gradle.kts
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2023-2024 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
+import elide.internal.conventions.elide
+import elide.internal.conventions.kotlin.KotlinTarget
+import elide.internal.conventions.publishing.publish
+
+plugins {
+  kotlin("multiplatform")
+  kotlin("plugin.serialization")
+
+  alias(libs.plugins.micronaut.library)
+  alias(libs.plugins.micronaut.graalvm)
+
+  id(libs.plugins.ksp.get().pluginId)
+  id("elide.internal.conventions")
+}
+
+elide {
+  publishing {
+    id = "server"
+    name = "Elide for Embedded"
+    description = "Embedded host interfaces for Elide applications."
+
+    publish("maven") {
+      from(components["kotlin"])
+    }
+  }
+
+  kotlin {
+    target = KotlinTarget.All
+    explicitApi = true
+  }
+
+  java {
+    configureModularity = false
+  }
+}
+
+group = "dev.elide"
+version = rootProject.version as String
+
+micronaut {
+  version = libs.versions.micronaut.lib.get()
+
+  processing {
+    incremental = true
+    annotations.addAll(listOf(
+      "elide.embedded",
+      "elide.embedded.*",
+      "elide.embedded.annotations",
+      "elide.embedded.annotations.*",
+    ))
+  }
+}
+
+dependencies {
+  // Nothing yet.
+
+  // Testing
+  testImplementation(projects.packages.test)
+  testImplementation(kotlin("test"))
+}

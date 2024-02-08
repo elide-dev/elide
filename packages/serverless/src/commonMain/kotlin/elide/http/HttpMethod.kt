7dd9ce27bbb2779d434dd5ccbd6ef2a7ad2ae0c5
Sam Gammon <sam@elide.ventures>
2024-02-07 17:59:45 -0800

    feat: initial `http` structure
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ packages/serverless/src/commonMain/kotlin/elide/http/HttpMethod.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2024 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
+package elide.http
+
+import kotlin.jvm.JvmInline
+import elide.http.api.StandardHttpMethod
+import elide.http.api.HttpMethod as HttpMethodAPI
+
+/**
+ *
+ */
+public sealed interface HttpMethod : HttpMethodAPI {
+  /**
+   * ## Standard HTTP method.
+   */
+  @JvmInline public value class Standard(public val method: StandardHttpMethod): HttpMethod
+
+  /**
+   * ## Custom HTTP method.
+   */
+  @JvmInline public value class Custom(public val name: String): HttpMethod
+}
---
+++ packages/serverless/src/nativeMain/kotlin/elide/http/MutableHttpRequest.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024 Elide Ventures, LLC.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
+
+package elide.http
+
+import elide.http.api.HttpMapping
+import elide.http.api.HttpMethod
+import elide.http.api.HttpString
+import elide.net.api.URL
+import elide.http.api.MutableHttpRequest as HttpRequestAPI
+
+/**
+ *
+ */
+public actual class MutableHttpRequest : MutableHttpMessage(), HttpRequestAPI {
+  actual override var method: HttpMethod = TODO("Not yet implemented")
+  actual override var url: URL get() = TODO("Not yet implemented")
+  actual override var path: HttpString = "/"
+  actual override var query: HttpMapping = MutableHttpMapping.empty()
+}

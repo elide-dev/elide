9f61b1a20b515eff17cbf3def521ca771d99e10b
Dario Valdespino <dvaldespino00@gmail.com>
2024-03-26 11:42:02 -0400

    fix(embedded): runtime configuration should be available from context.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/embedded/src/main/kotlin/elide/embedded/EmbeddedRuntimeContext.kt
+++ packages/embedded/src/main/kotlin/elide/embedded/EmbeddedRuntimeContext.kt
@@ -2,6 +2,9 @@ package elide.embedded
 
 /** A specialized DI container used by the embedded runtime to resolve core services. */
 public interface EmbeddedRuntimeContext {
+  /** Runtime configuration used by this context. */
+  public val configuration: EmbeddedConfiguration
+
   /** The [EmbeddedAppRegistry] used by the runtime. */
   public val appRegistry: EmbeddedAppRegistry
 }

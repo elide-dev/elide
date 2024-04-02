67bf3b765f31b3ded97f61187017f610058e3788
Dario Valdespino <dvaldespino00@gmail.com>
2024-04-02 13:20:44 -0700

    chore(embedded): refactor C typedefs.

--- packages/embedded/src/main/native/elide_embedded_types.h
+++ packages/embedded/src/main/native/elide_embedded_types.h
@@ -8,45 +8,45 @@
 #define ELIDE_ERR_ALREADY_INITIALIZED 3
 
 /* Defines the version of the interop protocol for the runtime. */
-enum elide_protocol_version_t {
+typedef enum {
   /* Defines version 1.0 of the embedded protocol. */
   V1_0,
-};
+} elide_protocol_version_t;
 
 /* Defines the format for exchanging data with the runtime. */
-enum elide_protocol_format_t {
+typedef enum {
   /* Selects the Protobuf format for data exchange in runtime operations. */
   PROTOBUF,
 
   /* Selects the Cap'n'Proto format for data exchange in runtime operations. */
   CAPNPROTO,
-};
+} elide_protocol_format_t;
 
 /* Defines the dispatch mode used by a guest app. */
-enum elide_app_mode_t {
+typedef enum {
   /*
    * Use a fetch-style invocation API, where guest code exposes a 'fetch'
    * function that handles incoming requests.
    */
   FETCH,
-};
+} elide_app_mode_t;
 
 /* Defines the programming language used by a guest application. */
-enum elide_app_lang_t {
+typedef enum {
   /* Use JavaScript as guest language. */
   JS,
 
   /* Use Python as guest language. */
   PYTHON,
-};
+} elide_app_lang_t;
 
 /* Configuration struct for the embedded runtime. */
 typedef struct elide_config_t {
   /* Dispatch protocol version. */
-  enum elide_protocol_version_t version;
+  elide_protocol_version_t version;
 
   /* Dispatch protocol serial format. */
-  enum elide_protocol_format_t format;
+  elide_protocol_format_t format;
 
   /** Path to the guest resources directory. */
   char *guest_root;
@@ -61,10 +61,10 @@ typedef struct elide_app_config_t {
   char *entrypoint;
 
   /* Language of the guest application. */
-  enum elide_app_lang_t language;
+  elide_app_lang_t language;
 
   /* Dispatch style for the application (e.g. 'fetch'). */
-  enum elide_app_mode_t mode;
+  elide_app_mode_t mode;
 } elide_app_config_t;
 
 #endif // ELIDE_EMBEDDED_H

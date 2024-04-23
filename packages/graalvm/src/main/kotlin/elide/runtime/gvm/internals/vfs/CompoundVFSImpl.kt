bccd12a77448ef5effb66450bc3621f72898039e
Sam Gammon <sam@elide.ventures>
2024-04-22 21:40:34 -0700

    feat(vfs): language-specific vfs layering
    - feat(vfs): implement compound vfs with primary and overlays
    - feat(vfs): prepare python/ruby language overlays
    - fix: re-enable vfs support even when python/ruby are active
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/vfs/CompoundVFSImpl.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/gvm/internals/vfs/CompoundVFSImpl.kt
@@ -13,6 +13,7 @@
 
 package elide.runtime.gvm.internals.vfs
 
+import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.net.URI
@@ -22,17 +23,48 @@ import java.nio.file.*
 import java.nio.file.DirectoryStream.Filter
 import java.nio.file.attribute.FileAttribute
 import elide.runtime.Logger
+import elide.runtime.gvm.internals.GuestVFS
+import elide.runtime.gvm.internals.LanguageVFS
 
 /**
  * TBD.
  */
-internal class CompoundVFSImpl (
+internal class CompoundVFSImpl private constructor (
   config: EffectiveGuestVFSConfig,
   private val primary: AbstractDelegateVFS<*>,
-  private val overlays: Array<AbstractDelegateVFS<*>>,
+  private val overlays: Array<GuestVFS>,
   private val hostAllowed: Boolean = false,
   private val hostSocketsAllowed: Boolean = false,
 ) : AbstractBaseVFS<CompoundVFSImpl>(config) {
+  companion object {
+    // Create a compound VFS implementation.
+    @JvmStatic fun create(
+      primary: AbstractDelegateVFS<*>,
+      overlays: List<GuestVFS>,
+      hostAllowed: Boolean,
+      hostSocketsAllowed: Boolean
+    ): CompoundVFSImpl = CompoundVFSImpl(
+      config = primary.config,
+      primary = primary,
+      overlays = overlays.toTypedArray(),
+      hostAllowed = hostAllowed,
+      hostSocketsAllowed = hostSocketsAllowed
+    )
+  }
+
+  private val inOrderCandidates: Array<GuestVFS> by lazy {
+    listOf(primary, *overlays)
+      .filter { it !is LanguageVFS }
+      .toTypedArray()
+  }
+
+  private val languageVfsOverlays: Array<LanguageVFS> by lazy {
+    arrayOf(primary, *overlays)
+      .filter { it is LanguageVFS }
+      .map { it as LanguageVFS }
+      .toTypedArray()
+  }
+
   override fun allowsHostFileAccess(): Boolean = hostAllowed || overlays.any {
     it.allowsHostFileAccess()
   }
@@ -45,44 +77,128 @@ internal class CompoundVFSImpl (
     primary.logging
   }
 
+  private inline fun <reified R> delegatePrimary(action: (AbstractDelegateVFS<*>) -> R): R {
+    primary.apply {
+      return action.invoke(this)
+    }
+  }
+
+  private inline fun <reified R> firstNonFailure(action: (GuestVFS) -> R): R? {
+    var first: Throwable? = null
+    inOrderCandidates.forEach {
+      try {
+        return action.invoke(it)
+      } catch (e: IOException) {
+        if (first == null) {
+          first = e
+        }
+      }
+    }
+    throw requireNotNull(first)
+  }
+
+  private inline fun <reified R> firstWritable(path: Path? = null, action: (GuestVFS) -> R): R? {
+    return inOrderCandidates.filter {
+      it.writable
+    }.firstNotNullOfOrNull {
+      try {
+        action.invoke(it)
+      } catch (ioe: IOException) {
+        null
+      }
+    }
+  }
+
+  private inline fun <reified R> firstWithPath(path: Path, action: (GuestVFS) -> R): R? {
+    val str = path.toString()
+    val languageVfsHandler = languageVfsOverlays.firstOrNull { it.accepts(path) }
+
+    // language resources should pre-empty
+    if (languageVfsHandler != null) {
+      return action.invoke(languageVfsHandler)
+    }
+    return inOrderCandidates.firstNotNullOfOrNull {
+      try {
+        (it to it.parsePath(str))
+      } catch (ioe: IOException) {
+        null
+      }
+    }?.let { (target, path) ->
+      if (Files.exists(path)) {
+        action.invoke(target)
+      } else {
+        null
+      }
+    }
+  }
+
+  private inline fun <reified R> firstForRead(path: Path, action: (GuestVFS) -> R): R? {
+    return firstWithPath(path) {
+      action.invoke(it)
+    }
+  }
+
+  private inline fun <reified R> firstForWrite(path: Path, action: (GuestVFS) -> R): R? {
+    return firstWritable(path) {
+      action.invoke(it)
+    }
+  }
+
   override fun close() {
-    TODO("")
+    inOrderCandidates.forEach {
+      try {
+        it.close()
+      } catch (ioe: IOException) {
+        // swallow
+      }
+    }
   }
 
-  override fun getSeparator(): String {
-    TODO("")
-  }
+  override val compound: Boolean get() = true
+  override val writable: Boolean get() = inOrderCandidates.any { it.writable }
+  override val deletable: Boolean get() = inOrderCandidates.any { it.deletable }
+  override val host: Boolean get() = inOrderCandidates.any { it.host }
+  override val virtual: Boolean get() = inOrderCandidates.any { it.virtual }
+  override val supportsSymlinks: Boolean get() = inOrderCandidates.any { it.supportsSymlinks }
 
-  override fun getPathSeparator(): String {
-    TODO("")
-  }
+  override fun getSeparator(): String = delegatePrimary { it.separator }
+  override fun getPathSeparator(): String = delegatePrimary { it.pathSeparator }
+  override fun parsePath(uri: URI): Path = delegatePrimary { it.parsePath(uri) }
+  override fun parsePath(path: String): Path = delegatePrimary { it.parsePath(path) }
+  override fun getPath(vararg segments: String): Path = delegatePrimary { it.getPath(*segments) }
 
-  override fun parsePath(uri: URI): Path {
-    TODO("")
-  }
+  override fun toAbsolutePath(path: Path): Path = firstWithPath(path) {
+    try {
+      it.toAbsolutePath(path)
+    } catch (pme: ProviderMismatchException) {
+      it.toAbsolutePath(it.parsePath(path.toString()))
+    }
+  } ?: path
 
-  override fun parsePath(path: String): Path {
-    TODO("")
-  }
-
-  override fun getPath(vararg segments: String): Path {
-    TODO("")
-  }
-
-  override fun toAbsolutePath(path: Path): Path {
-    TODO("")
-  }
-
-  override fun toRealPath(path: Path, vararg linkOptions: LinkOption): Path {
-    TODO("")
-  }
+  override fun toRealPath(path: Path, vararg linkOptions: LinkOption): Path =
+    delegatePrimary { it.toRealPath(path, *linkOptions) }
 
   override fun checkAccess(path: Path, modes: MutableSet<out AccessMode>, vararg linkOptions: LinkOption) {
-    TODO("")
+    var preserved: GuestIOAccessDenied? = null
+    inOrderCandidates.forEach {
+      try {
+        it.checkAccess(path, modes, *linkOptions)
+      } catch (accessDenied: GuestIOAccessDenied) {
+        if (preserved == null) {
+          preserved = accessDenied
+        }
+        // continue
+      }
+    }
+    if (preserved != null) {
+      throw requireNotNull(preserved)
+    }
   }
 
   override fun createDirectory(dir: Path, vararg attrs: FileAttribute<*>) {
-    TODO("")
+    firstForWrite(dir) {
+      it.createDirectory(dir, *attrs)
+    }
   }
 
   override fun newByteChannel(
@@ -90,74 +206,126 @@ internal class CompoundVFSImpl (
     options: MutableSet<out OpenOption>,
     vararg attrs: FileAttribute<*>
   ): SeekableByteChannel {
-    TODO("")
+    val languageMatch = languageVfsOverlays.firstNotNullOfOrNull {
+      if (!it.accepts(path)) null else it
+    }
+    if (languageMatch != null) {
+      return languageMatch.newByteChannel(
+        path,
+        options,
+        *attrs,
+      )
+    }
+
+    return firstForRead(path) {
+      it.newByteChannel(path, options, *attrs)
+    } ?: throw NoSuchFileException(path.toString())
   }
 
   override fun newDirectoryStream(dir: Path, filter: Filter<in Path>): DirectoryStream<Path> {
-    TODO("")
+    return firstForRead(dir) {
+      it.newDirectoryStream(dir, filter)
+    } ?: throw NoSuchFileException(dir.toString())
   }
 
   override fun readAttributes(path: Path, attributes: String, vararg options: LinkOption): MutableMap<String, Any> {
-    TODO("")
+    return firstForRead(path) {
+      it.readAttributes(path, attributes, *options)
+    } ?: throw NoSuchFileException(path.toString())
   }
 
   override fun setAttribute(path: Path, attribute: String, value: Any, vararg options: LinkOption) {
-    super.setAttribute(path, attribute, value, *options)
+    firstForWrite(path) {
+      it.setAttribute(path, attribute, value, *options)
+    }
   }
 
   override fun copy(source: Path, target: Path, vararg options: CopyOption?) {
-    super.copy(source, target, *options)
+    firstForRead(source) {
+      val src = this
+      firstForWrite(target) {
+        require(src === this) {
+          "Source and target must be from the same VFS"
+        }
+        it.copy(source, target, *options)
+      }
+    }
   }
 
   override fun move(source: Path, target: Path, vararg options: CopyOption?) {
-    super.move(source, target, *options)
+    firstForRead(source) {
+      val src = this
+      firstForWrite(target) {
+        require(src === this) {
+          "Source and target must be from the same VFS"
+        }
+        it.move(source, target, *options)
+      }
+    }
   }
 
   override fun delete(path: Path) {
-    TODO("")
+    firstForWrite(path) {
+      it.delete(path)
+    }
   }
 
   override fun createLink(link: Path, existing: Path) {
-    super.createLink(link, existing)
+    firstForWrite(link) {
+      it.createLink(link, existing)
+    }
   }
 
   override fun createSymbolicLink(link: Path, target: Path, vararg attrs: FileAttribute<*>?) {
-    super.createSymbolicLink(link, target, *attrs)
+    firstForWrite(link) {
+      it.createSymbolicLink(link, target, *attrs)
+    }
   }
 
   override fun readSymbolicLink(link: Path): Path {
-    return super.readSymbolicLink(link)
+    return firstForRead(link) {
+      readSymbolicLink(link)
+    } ?: throw NoSuchFileException(link.toString())
   }
 
   override fun setCurrentWorkingDirectory(currentWorkingDirectory: Path) {
-    super.setCurrentWorkingDirectory(currentWorkingDirectory)
+    firstForWrite(currentWorkingDirectory) {
+      setCurrentWorkingDirectory(currentWorkingDirectory)
+    }
   }
 
   override fun getMimeType(path: Path): String? {
-    return super.getMimeType(path)
+    return delegatePrimary {
+      it.getMimeType(path)
+    }
   }
 
   override fun getEncoding(path: Path): Charset {
-    return super.getEncoding(path)
+    return delegatePrimary { it.getEncoding(path) }
   }
 
-  override fun getTempDirectory(): Path {
-    return super.getTempDirectory()
-  }
+  override fun getTempDirectory(): Path =
+    firstWritable { tempDirectory  } ?: throw IOException("No writable VFS found")
 
   override fun isSameFile(path1: Path, path2: Path, vararg options: LinkOption): Boolean {
-    return super.isSameFile(path1, path2, *options)
+    return firstForRead(path1) {
+      it.isSameFile(path1, path2, *options)
+    } ?: throw NoSuchFileException(path1.toString())
   }
 
   override fun readStream(path: Path, vararg options: OpenOption): InputStream {
-    TODO("")
+    return delegatePrimary {
+      it.readStream(path, *options)
+    }
   }
 
   override fun writeStream(path: Path, vararg options: OpenOption): OutputStream {
-    TODO("")
+    return delegatePrimary {
+      it.writeStream(path, *options)
+    }
   }
 
   override fun checkPolicy(request: AccessRequest): AccessResponse {
-    TODO("")
+    return delegatePrimary { it.checkPolicy(request) }
   }
 }

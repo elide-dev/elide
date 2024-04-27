fbd8e28613f302cd6667e3b62677e11de69d9102
Dario Valdespino <dvaldespino00@gmail.com>
2024-04-27 13:17:08 -0400

    feat(embedded): remove wip implementations.
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/embedded/src/main/kotlin/elide/embedded/http/EmbeddedRequest.kt
+++ packages/embedded/src/main/kotlin/elide/embedded/http/EmbeddedRequest.kt
@@ -9,13 +9,4 @@ import elide.vm.annotations.Polyglot
  * over a native boundary. The exchange mechanism may influence the behavior of certain methods and properties, but
  * the general contract is kept regardless of the provenance.
  */
-public interface EmbeddedRequest {
-  /** The request URI string. */
-  @get:Polyglot public val uri: String
-
-  /** The HTTP method of the request. */
-  @get:Polyglot public val method: String
-
-  /** A multi-map holding values for the request headers. */
-  @get:Polyglot public val headers: EmbeddedHeaders
-}
+public interface EmbeddedRequest
--- packages/embedded/src/main/kotlin/elide/embedded/http/EmbeddedResponse.kt
+++ packages/embedded/src/main/kotlin/elide/embedded/http/EmbeddedResponse.kt
@@ -9,13 +9,4 @@ import elide.vm.annotations.Polyglot
  * over a native boundary. The exchange mechanism may influence the behavior of certain methods and properties, but
  * the general contract is kept regardless of the provenance.
  */
-public interface EmbeddedResponse {
-  /** A mutable status code for the response. */
-  @get:Polyglot @set:Polyglot public var statusCode: Int
-
-  /** A mutable status message for the response. */
-  @get:Polyglot @set:Polyglot public var statusMessage: String
-
-  /** A mutable multi-map for the response headers. */
-  @get:Polyglot public val headers: MutableEmbeddedHeaders
-}
+public interface EmbeddedResponse

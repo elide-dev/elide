8832ba3ae17a9ec348fd417ae84f98dc42fd951b
Sam Gammon <sam@elide.ventures>
2024-05-07 18:57:11 -0700

    feat(url): implement support for `URLSearchParams`
    - feat(url): implement `URLSearchParams`
    - feat(node): implement initial `url` module support
    - test: initial tests for `URLSearchParams` (host-side)
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/URLSearchParams.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/URLSearchParams.kt
@@ -13,7 +13,61 @@
 
 package elide.runtime.intrinsics.js
 
-/** TBD. */
-public interface URLSearchParams : java.io.Serializable, MultiMapLike<String, String> {
-  // Not yet implemented.
+import org.graalvm.polyglot.proxy.ProxyHashMap
+import org.graalvm.polyglot.proxy.ProxyObject
+import elide.annotations.API
+
+/**
+ * Object properties expected to be present on [URLSearchParams].
+ */
+internal val URL_SEARCH_PARAMS_PROPERTIES = arrayOf(
+  "size",
+  "entries",
+  "forEach",
+  "get",
+  "getAll",
+  "has",
+  "keys",
+  "toString",
+  "values",
+)
+
+/**
+ * Object properties expected to be present on [MutableURLSearchParams].
+ */
+internal val URL_SEARCH_PARAMS_MUTABLE_PROPERTIES = URL_SEARCH_PARAMS_PROPERTIES.plus(arrayOf(
+  "append",
+  "delete",
+  "set",
+  "sort",
+))
+
+/**
+ * # JavaScript: URL Search Parameters
+ *
+ * The `URLSearchParams` interface and class is universally supported across most JavaScript and browser environments,
+ * and models query (`GET`-method) parameters expressed within a URL.
+ *
+ * URL search parameters are typically associated with, and made available from, [URL] instances.
+ *
+ * Note that parameters can be specified multiple times in a URL; as a result, this structure behaves as [MultiMapLike].
+ *
+ * &nbsp;
+ *
+ * ## Mutability
+ *
+ * For mutable URL search params, this interface is extended into [MutableURLSearchParams].
+ */
+@API public interface URLSearchParams :
+  java.io.Serializable,
+  MultiMapLike<String, String>,
+  ProxyHashMap,
+  ProxyObject {
+  override fun getMemberKeys(): Array<String> = URL_SEARCH_PARAMS_PROPERTIES
+  override fun hasMember(key: String): Boolean = key in URL_SEARCH_PARAMS_PROPERTIES
+
+  /**
+   * Sort the parameters in-place.
+   */
+  public fun sort()
 }

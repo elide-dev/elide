1175a131794fb0e3c87fe2c2517d6b520e5e0bcb
Sam Gammon <sam@elide.ventures>
2024-05-08 13:56:34 -0700

    chore: js bench tooling with `mitata`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

---
+++ tools/bench/js/basic.mjs
@@ -0,0 +1,32 @@
+import { run, bench, group, baseline } from 'mitata';
+
+// deno
+// import { ... } from 'npm:mitata';
+
+// d8/jsc
+// import { ... } from '<path to mitata>/src/cli.mjs';
+
+bench('noop', () => {});
+bench('noop2', () => {});
+
+group('group', () => {
+  baseline('baseline', () => {});
+  bench('Date.now()', () => Date.now());
+  bench('performance.now()', () => performance.now());
+});
+
+group({ name: 'group2', summary: false }, () => {
+  bench('new Array(0)', () => new Array(0));
+  bench('new Array(1024)', () => new Array(1024));
+});
+
+await run({
+  units: false, // print small units cheatsheet
+  silent: false, // enable/disable stdout output
+  avg: true, // enable/disable avg column (default: true)
+  json: false, // enable/disable json output (default: false)
+  colors: true, // enable/disable colors (default: true)
+  min_max: true, // enable/disable min/max column (default: true)
+  percentiles: true, // enable/disable percentiles column (default: true)
+});
+

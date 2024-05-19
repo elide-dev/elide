619b5fb15c800e2b68a89ecfbfd9d27a8a2297d0
Sam Gammon <sam@elide.ventures>
2024-05-19 14:54:44 -0700

    chore: upgrade micronaut → `4.4.x`
    - chore: upgrade micronaut
    - chore: re-gen lockfiles
    - chore: update verification metadata
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,10 +11,6 @@
  *  License for the specific language governing permissions and limitations under the License.
  */
 
-//
-// Elide
-//
-
 @file:Suppress(
   "UnstableApiUsage",
   "DSL_SCOPE_VIOLATION",

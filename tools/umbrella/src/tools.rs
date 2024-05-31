7e8382e5b534d5b6dc13d9f2e1655b248e8b70d3
Sam Gammon <sam@elide.ventures>
2024-05-31 12:09:43 -0700

    feat: initial diagnostics model
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- tools/umbrella/src/tools.rs
+++ tools/umbrella/src/tools.rs
@@ -2,9 +2,47 @@ use serde::{Deserialize, Serialize};
 
 #[typeshare::typeshare]
 #[derive(Clone, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub enum ToolType {
+  Linter,
+  Compiler,
+}
+
+#[typeshare::typeshare]
+#[derive(Clone, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub struct ToolInfo {
   pub name: &'static str,
   pub version: &'static str,
   pub language: &'static str,
   pub experimental: bool,
+  pub kind: ToolType,
 }
+
+// Library version of the tooling layer.
+pub static LIB_VERSION: &'static str = "0.1.0";
+
+// API version of the tooling layer.
+pub static API_VERSION: &'static str = "v1";
+
+pub static UV_INFO: ToolInfo = ToolInfo {
+  name: "uv",
+  version: "0.1.9",
+  language: "python",
+  experimental: true,
+  kind: ToolType::Linter,
+};
+
+pub static RUFF_INFO: ToolInfo = ToolInfo {
+  name: "ruff",
+  version: "0.4.0",
+  language: "python",
+  experimental: true,
+  kind: ToolType::Linter,
+};
+
+pub static OXY_INFO: ToolInfo = ToolInfo {
+  name: "oxy",
+  version: "0.12.3",
+  language: "js",
+  experimental: false,
+  kind: ToolType::Compiler,
+};

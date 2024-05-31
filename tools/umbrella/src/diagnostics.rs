8eb5540b7f60fb8a261d5c357c145d4773c50cc7
Sam Gammon <sam@elide.ventures>
2024-05-31 12:09:43 -0700

    feat: initial diagnostics model
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- tools/umbrella/src/diagnostics.rs
+++ tools/umbrella/src/diagnostics.rs
@@ -2,7 +2,27 @@ use serde::{Deserialize, Serialize};
 
 #[typeshare::typeshare]
 #[derive(Clone, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub enum Severity {
+  Info,
+  Warning,
+  Error
+}
+
+#[typeshare::typeshare]
+#[derive(Clone, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
+pub struct CodeLocation {
+  pub file: &'static str,
+  pub line: u32,
+  pub column: u32
+}
+
+#[typeshare::typeshare]
+#[derive(Clone, Hash, Eq, PartialEq, Debug, Serialize, Deserialize)]
 pub struct DiagnosticNote {
   pub id: &'static str,
-  pub message: &'static str
+  pub tool: &'static str,
+  pub code: &'static str,
+  pub message: &'static str,
+  pub location: CodeLocation,
+  pub severity: Severity
 }

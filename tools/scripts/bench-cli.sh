dd363f81d34c13b9206c7dbeb3e08e2c0b3a1c99
Sam Gammon <sam@elide.ventures>
2024-06-06 02:02:40 -0700

    chore: run prettier
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- tools/scripts/bench-cli.sh
+++ tools/scripts/bench-cli.sh
@@ -8,12 +8,12 @@ ELIDE="$1"
 HYPERFINE_ARGS="--shell none"
 
 # run `--help`
-hyperfine $HYPERFINE_ARGS "$ELIDE --help";
+hyperfine $HYPERFINE_ARGS "$ELIDE --help"
 
 # run some hello scripts
-hyperfine $HYPERFINE_ARGS -n hello-js "$ELIDE run --javascript ./tools/scripts/hello.js" -n hello-ts "$ELIDE run --typescript ./tools/scripts/hello.ts";
+hyperfine $HYPERFINE_ARGS -n hello-js "$ELIDE run --javascript ./tools/scripts/hello.js" -n hello-ts "$ELIDE run --typescript ./tools/scripts/hello.ts"
 
 # run some sqlite stuff
-hyperfine $HYPERFINE_ARGS -n sqlite "$ELIDE run --javascript ./tools/scripts/sqlite.js";
+hyperfine $HYPERFINE_ARGS -n sqlite "$ELIDE run --javascript ./tools/scripts/sqlite.js"
 
-echo "CLI bench done.";
+echo "CLI bench done."

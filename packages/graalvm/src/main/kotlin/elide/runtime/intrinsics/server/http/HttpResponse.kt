e87a52f6fa615028f0a49d2fbbe10c7324f56e90
Sam Gammon <sam@elide.ventures>
2024-06-06 12:55:38 -0700

    fix(server): allow setting response headers
    The Netty response intrinsic provided to guests is missing a method
    to set headers on the response. While we're there, I can add some
    other methods users may need.
    
    - fix: header methods on response intrinsic
    - chore: re-pin `graalvm` module
    - chore: update `graalvm` detekt baseline
    - chore: more gitattribute fixes
    
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/server/http/HttpResponse.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/server/http/HttpResponse.kt
@@ -10,17 +10,43 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under the License.
  */
-
 package elide.runtime.intrinsics.server.http
 
-import org.graalvm.polyglot.HostAccess.Export
+import elide.annotations.API
 import elide.runtime.core.DelicateElideApi
 import elide.runtime.core.PolyglotValue
+import elide.vm.annotations.Polyglot
 
 /** Represents an HTTP response returned by the server, accessible from guest code. */
-@DelicateElideApi public interface HttpResponse {
+@API @DelicateElideApi public interface HttpResponse: ExpressResponseAPI {
+  /**
+   * Provide a header to the response; this method is exported to guest code.
+   *
+   * Note that headers must be provided before [send] is called.
+   *
+   * @param name The name of the header to set.
+   * @param value The value of the header to set.
+   */
+  @Polyglot public fun header(name: String, value: String)
+
   /**
    * Exported method allowing guest code to send a response to the client with the given [status] code and [body].
+   *
+   * @param status The HTTP status code to send.
+   * @param body The body of the response to send.
    */
-  @Export public fun send(status: Int, body: PolyglotValue?)
+  @Polyglot override fun send(status: Int, body: PolyglotValue?)
+
+  /**
+   * Append a header to the response; this method is exported to guest code.
+   *
+   * Note that headers must be provided before [send] is called.
+   * This method will add a header to the response even if there is already a header by the same name.
+   *
+   * @param name The name of the header to set.
+   * @param value The value of the header to set.
+   */
+  @Polyglot override fun append(name: String, value: String?) {
+    header(name, value ?: "")
+  }
 }
--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/server/http/netty/NettyHttpResponse.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/server/http/netty/NettyHttpResponse.kt
@@ -15,23 +15,96 @@ package elide.runtime.intrinsics.server.http.netty
 import io.netty.buffer.Unpooled
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.http.*
-import org.graalvm.polyglot.HostAccess.Export
+import org.graalvm.polyglot.Value
+import org.graalvm.polyglot.proxy.ProxyExecutable
+import org.graalvm.polyglot.proxy.ProxyObject
+import java.text.DateFormat
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
 import elide.runtime.core.DelicateElideApi
 import elide.runtime.core.PolyglotValue
+import elide.runtime.gvm.internals.intrinsics.js.JsError
 import elide.runtime.intrinsics.server.http.HttpRequest
 import elide.runtime.intrinsics.server.http.HttpResponse
+import elide.vm.annotations.Polyglot
+
+// Methods and properties exposed to guest code on responses.
+private val NETTY_HTTP_RESPONSE_PROPS_AND_METHODS = arrayOf(
+  "append",
+  "end",
+  "get",
+  "header",
+  "send",
+  "set",
+  "status",
+)
 
 /** [HttpRequest] implementation wrapping a Netty handler context. */
-@DelicateElideApi internal class NettyHttpResponse(private val context: ChannelHandlerContext) : HttpResponse {
+@DelicateElideApi internal class NettyHttpResponse(
+  private val context: ChannelHandlerContext,
+  private val includeDefaults: Boolean = true,
+)
+  : HttpResponse, ProxyObject {
   /** Whether the response has already been sent. */
   private val sent = AtomicBoolean(false)
+
+  /** Whether the response has already been sent. */
+  private val body = AtomicReference<PolyglotValue>(null)
+
+  /** Explicit status set by guest code. */
+  private val status = AtomicInteger(0)
+
+  /** The HTTP version of the response. */
+  @Volatile private var httpVersion = HttpVersion.HTTP_1_1
 
   /** Headers for this response, dispatched once the response is sent to the client. */
   private val headers = DefaultHttpHeaders()
 
-  @Export override fun send(status: Int, body: PolyglotValue?) {
-    send(HttpResponseStatus.valueOf(status), body)
+  init {
+    // prepare headers
+    headers
+      .set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.TEXT_PLAIN)
+      .set(HttpHeaderNames.SERVER, "elide")
+  }
+
+  @Polyglot override fun header(name: String, value: String) {
+    headers.set(name, value)
+  }
+
+  @Polyglot override fun set(name: String, value: String?) {
+    headers.set(name, value)
+  }
+
+  @Polyglot override fun get(name: String): String? {
+    return headers.get(name)
+  }
+
+  @Polyglot override fun append(name: String, value: String?) {
+    headers.add(name, value)
+  }
+
+  @Polyglot override fun status(status: Int) {
+    this.status.set(status)
+  }
+
+  @Polyglot override fun send(status: Int, body: PolyglotValue?) {
+    status(status)
+    this.body.set(body)
+    end()
+  }
+
+  @Polyglot override fun end() {
+    val status = status.get()
+    val effective = if (status == 0) HttpResponseStatus.OK.code() else status
+    send(HttpResponseStatus.valueOf(effective), body.get())
+  }
+
+  // Fills response headers expected by-spec.
+  private fun fillResponseHeaders() {
+    if (includeDefaults) {
+      headers.set(HttpHeaderNames.DATE, DateFormat.getInstance().format(System.currentTimeMillis()))
+    }
   }
 
   /**
@@ -47,17 +120,14 @@ import elide.runtime.intrinsics.server.http.HttpResponse
     // treat any type of value as a string (force conversion)
     val content = body?.let { Unpooled.wrappedBuffer(it.toString().toByteArray()) } ?: Unpooled.EMPTY_BUFFER
 
-    // prepare headers
-    headers
-      .set(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.TEXT_PLAIN)
-      .set(HttpHeaderNames.SERVER, "Elide")
-
     // prepare the response object
+    fillResponseHeaders()
+
     val response = if (content != null) {
       headers.set(HttpHeaderNames.CONTENT_LENGTH, content.writerIndex())
 
       DefaultFullHttpResponse(
-        /* version = */ HttpVersion.HTTP_1_1,
+        /* version = */ httpVersion,
         /* status = */ status,
         /* content = */ content,
         /* headers = */ headers,
@@ -65,7 +135,7 @@ import elide.runtime.intrinsics.server.http.HttpResponse
       )
     } else {
       DefaultHttpResponse(
-        /* version = */ HttpVersion.HTTP_1_1,
+        /* version = */ httpVersion,
         /* status = */ status,
         /* headers = */ headers,
       )
@@ -73,5 +143,76 @@ import elide.runtime.intrinsics.server.http.HttpResponse
 
     // send the response
     context.write(response)
+  }
+
+  override fun getMemberKeys(): Array<String> = NETTY_HTTP_RESPONSE_PROPS_AND_METHODS
+  override fun hasMember(key: String?): Boolean = key != null && key in NETTY_HTTP_RESPONSE_PROPS_AND_METHODS
+  override fun putMember(key: String?, value: Value?) {
+    // no-op
+  }
+
+  override fun removeMember(key: String?): Boolean {
+    return false  // not supported
+  }
+
+  override fun getMember(key: String?): Any? = when (key) {
+    "get" -> ProxyExecutable {
+      val name = it.getOrNull(0)
+      when {
+        name == null || !name.isString -> throw JsError.valueError("Header name must be a string")
+        else -> get(name.asString())
+      }
+    }
+
+    "set" -> ProxyExecutable {
+      val name = it.getOrNull(0)
+      val value = it.getOrNull(1)
+      when {
+        name == null || !name.isString -> throw JsError.valueError("Header name must be a string")
+        value == null -> throw JsError.typeError("Header value is required")
+        else -> set(name.asString(), value.asString())
+      }
+    }
+
+    "append" -> ProxyExecutable {
+      val name = it.getOrNull(0)
+      val value = it.getOrNull(1)
+      when {
+        name == null || !name.isString -> throw JsError.valueError("Header name must be a string")
+        value == null -> throw JsError.typeError("Header value is required")
+        else -> append(name.asString(), value.asString())
+      }
+    }
+
+    "status" -> ProxyExecutable {
+      val status = it.getOrNull(0)
+      when {
+        status == null || !status.fitsInInt() -> throw JsError.typeError("Status code must be an integer")
+        else -> status(status.asInt())
+      }
+    }
+
+    "header" -> ProxyExecutable {
+      val name = it.getOrNull(0)
+      val value = it.getOrNull(1)
+      when {
+        name == null || !name.isString -> throw JsError.valueError("Header name must be a string")
+        value == null -> throw JsError.typeError("Header value is required")
+        else -> header(name.asString(), value.asString())
+      }
+    }
+
+    "send" -> ProxyExecutable {
+      val status = it.getOrNull(0)
+      val body = it.getOrNull(1)
+      when {
+        status == null || !status.fitsInInt() -> throw JsError.typeError("Status code must be an integer")
+        else -> send(status.asInt(), body)
+      }
+    }
+
+    "end" -> ProxyExecutable { this.end() }
+
+    else -> null
   }
 }

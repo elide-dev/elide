a5a9010e50125286b9046ce41e94f48f7d165847
Sam Gammon <sam@elide.ventures>
2024-06-18 00:20:06 -0700

    chore: cleanup for `HostPlatform` changes
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- packages/engine/src/main/kotlin/elide/runtime/core/HostPlatform.kt
+++ packages/engine/src/main/kotlin/elide/runtime/core/HostPlatform.kt
@@ -62,17 +62,17 @@ import elide.runtime.core.HostPlatform.OperatingSystem.*
     private const val STANDARD_SEPARATOR = '_'
 
     /** Parse a generic OS name, such as the value obtained from the 'os.name' system property. */
-    public fun parseOperatingSystem(name: String): OperatingSystem = when {
-      name == "linux" -> LINUX
-      name == "windows" -> WINDOWS
-      name == "darwin" || name == "mac os x" -> DARWIN
+    public fun parseOperatingSystem(name: String): OperatingSystem = when (name) {
+      "linux" -> LINUX
+      "windows" -> WINDOWS
+      "darwin", "mac os x", "macos" -> DARWIN
       else -> error("Unrecognized Operating System family: $name")
     }
 
     /** Parse a generic architecture name, such as the value obtained from the 'os.arch' system property. */
-    public fun parseArchitecture(name: String): Architecture = when {
-      name == "x86_64" || name == "x86-64" || name == "amd64" -> Architecture.AMD64
-      name == "arm64" || name == "aarch64" -> Architecture.ARM64
+    public fun parseArchitecture(name: String): Architecture = when (name) {
+      "x86_64", "x86-64", "amd64" -> Architecture.AMD64
+      "arm64", "aarch64" -> Architecture.ARM64
       else -> error("Unrecognized architecture: $name")
     }
 

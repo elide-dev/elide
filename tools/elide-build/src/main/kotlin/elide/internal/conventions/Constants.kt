f7db96a7927322140b0ced237b88622079af13bb
Sam Gammon <sam@elide.ventures>
2024-06-23 16:40:30 -0700

    chore: artifact version bump → `1.0.0-alpha10`
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- tools/elide-build/src/main/kotlin/elide/internal/conventions/Constants.kt
+++ tools/elide-build/src/main/kotlin/elide/internal/conventions/Constants.kt
@@ -216,7 +216,7 @@ public object Constants {
     private const val MAJOR_VERSION = "1.0.0"
 
     /** Major library version tag. */
-    private const val VERSION_TAG = "alpha9"
+    private const val VERSION_TAG = "alpha10"
 
     /** Version string for the library. */
     public const val VERSION: String = "$MAJOR_VERSION-$VERSION_TAG"

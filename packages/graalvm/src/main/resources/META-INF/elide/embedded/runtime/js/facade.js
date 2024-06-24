99b6533167855c227dc6ca494559949d85b11d0f
Sam Gammon <sam@elide.dev>
2024-06-23 17:38:58 -0700

    fix: package of release fixes for `linux-amd64`
    Signed-off-by: GitHub <noreply@github.com>

--- packages/graalvm/src/main/resources/META-INF/elide/embedded/runtime/js/facade.js
+++ packages/graalvm/src/main/resources/META-INF/elide/embedded/runtime/js/facade.js
@@ -1,3 +1,3 @@
-(function(){var i=globalThis,o=primordials.NativeValueError,a=["ValueError"],l=this||self;a[0]in l||typeof l.a=="undefined"||l.a("var "+a[0]);for(var r;a.length&&(r=a.shift());)a.length||o===void 0?l[r]&&l[r]!==Object.prototype[r]?l=l[r]:l=l[r]={}:l[r]=o;i.ValueError=o,globalThis.window=void 0,globalThis.gc=null}).call({});
+(function(){globalThis.window=void 0,globalThis.gc=null}).call({});
 // Elide JS Runtime. Copyright (c) 2023, Sam Gammon and the Elide Project Authors. All rights reserved.
 // Components of this software are licensed separately. See https://github.com/elide-dev/elide for more.

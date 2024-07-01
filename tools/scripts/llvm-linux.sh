941d723fcd312270732491336b7adb0791838c0c
Sam Gammon <sam@elide.ventures>
2024-07-01 16:17:33 -0700

    chore: fmt with prettier
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- tools/scripts/llvm-linux.sh
+++ tools/scripts/llvm-linux.sh
@@ -2,18 +2,17 @@
 
 LLVM_VERSION="18"
 
-echo "" && echo "Installing initial dependencies...";
+echo "" && echo "Installing initial dependencies..."
 apt-get update && apt install -y \
-	ca-certificates curl wget build-essential lsb-release software-properties-common gnupg;
+  ca-certificates curl wget build-essential lsb-release software-properties-common gnupg
 
-echo "" && echo "Installing LLVM $LLVM_VERSION...";
+echo "" && echo "Installing LLVM $LLVM_VERSION..."
 wget https://apt.llvm.org/llvm.sh \
-	&& chmod +x llvm.sh \
-	&& ./llvm.sh $LLVM_VERSION all;
+  && chmod +x llvm.sh \
+  && ./llvm.sh $LLVM_VERSION all
 
-echo "" && echo "Finalizing software...";
-apt-get update && apt-get upgrade -y;
+echo "" && echo "Finalizing software..."
+apt-get update && apt-get upgrade -y
 
-/usr/lib/llvm-18/bin/clang --version;
-echo "LLVM $LLVM_VERSION is ready.";
-
+/usr/lib/llvm-18/bin/clang --version
+echo "LLVM $LLVM_VERSION is ready."

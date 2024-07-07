5f46db74c61f91a124e2bd1198231b4394134525
Sam Gammon <sam@elide.ventures>
2024-07-07 14:18:51 -0700

    fixup! use of feature on stable
    Signed-off-by: Sam Gammon <sam@elide.ventures>

--- crates/sqlite/build.rs
+++ crates/sqlite/build.rs
@@ -10,7 +10,6 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under the License.
  */
-#![feature(exit_status_error)]
 
 use bindgen::Builder;
 use builder::{build_bindings, build_dual_cc, header_file, setup_cc, src_file};

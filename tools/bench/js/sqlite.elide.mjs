bb64a2f890331db30bb33b5913f6b9ee48e93dd7
Sam Gammon <sam@elide.dev>
2025-01-18 18:01:46 -0800

    chore: fmt
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- tools/bench/js/sqlite.elide.mjs
+++ tools/bench/js/sqlite.elide.mjs
@@ -1,9 +1,8 @@
-import { Database } from "sqlite";
+import { Database } from "sqlite"
 
-const db = new Database();
-db.exec("CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT)");
-db.exec("INSERT INTO cats (name) VALUES ('Bebe')");
-db.exec("INSERT INTO cats (name) VALUES ('Little Man')");
-const cats = db.query("SELECT * FROM cats ORDER BY name;").all();
-console.info("cats:", cats);
-
+const db = new Database()
+db.exec("CREATE TABLE cats (id INTEGER PRIMARY KEY, name TEXT)")
+db.exec("INSERT INTO cats (name) VALUES ('Bebe')")
+db.exec("INSERT INTO cats (name) VALUES ('Little Man')")
+const cats = db.query("SELECT * FROM cats ORDER BY name;").all()
+console.info("cats:", cats)

f155a92134140f99315c30654be618dbf6db0816
Sam Gammon <sam@elide.dev>
2025-03-10 00:05:34 -0700

    chore: cleanup for release pt 2
    - chore: move `creative` and `docs` dirs to new project dir
    - test: add smoke test runs for code in `tools/scripts`
    
    Signed-off-by: Sam Gammon <sam@elide.dev>

---
+++ packages/cli/src/test/kotlin/elide/tool/cli/AbstractEntryTest.kt
@@ -0,0 +1,59 @@
+package elide.tool.cli
+
+import io.micronaut.configuration.picocli.PicocliRunner
+import org.apache.commons.io.output.ByteArrayOutputStream
+import java.io.PrintStream
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.concurrent.atomic.AtomicReference
+import jakarta.inject.Inject
+
+abstract class AbstractEntryTest {
+  val testRuby = false
+  val testPython = false
+
+  companion object {
+    @JvmStatic protected val rootProjectPath: Path = Paths.get(System.getProperty("user.dir"))
+      .parent
+      .parent
+
+    @JvmStatic  protected val testScriptsPath: Path = rootProjectPath
+      .resolve("tools")
+      .resolve("scripts")
+      .toAbsolutePath()
+  }
+
+  @Inject lateinit var tool: Elide
+
+  protected fun assertToolExitsWithCode(expected: Int, vararg args: String) {
+    fun block(): Int =
+      PicocliRunner.execute(Elide::class.java, *args)
+
+    // capture stdout and stderr
+    val stubbedOut = ByteArrayOutputStream()
+    val stubbedErr = ByteArrayOutputStream()
+    val originalOut = System.out
+    val originalErr = System.err
+    System.setOut(PrintStream(stubbedOut))
+    System.setErr(PrintStream(stubbedErr))
+    val capturedOut: AtomicReference<ByteArray> = AtomicReference(null)
+    val capturedErr: AtomicReference<ByteArray> = AtomicReference(null)
+
+    val code = try {
+      block()
+    } finally {
+      capturedOut.set(stubbedOut.toByteArray())
+      capturedErr.set(stubbedErr.toByteArray())
+      System.setOut(originalOut)
+      System.setErr(originalErr)
+    }
+    assert(expected == code) {
+      "should exit with code $expected, but got $code;\n" +
+      "stdout: ${String(capturedOut.get())}\nstderr: ${String(capturedErr.get())}"
+    }
+  }
+
+  protected fun assertToolRunsWith(vararg args: String) {
+    assertToolExitsWithCode(0, *args)
+  }
+}

6216cbd8a9b48e0f1e9ce2b9624e9aa22bb49b8f
Sam Gammon <sam@elide.dev>
2025-03-10 00:45:57 -0700

    fix: issues with fetch globals
    Signed-off-by: Sam Gammon <sam@elide.dev>

---
+++ tools/scripts/fetch.mjs
@@ -0,0 +1,8 @@
+console.log("Fetch: ", Fetch)
+console.log("Headers: ", Headers)
+console.log("Request: ", Request)
+console.log("Response: ", Response)
+console.log("fetch: ", fetch)
+
+const resp = await fetch("https://google.com/")
+console.log("google: ", resp)

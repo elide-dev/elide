53faf97770eb54f29868fb111705fc7259bbac1d
Sam Gammon <sam@elide.dev>
2025-03-24 19:53:56 -0700

    fix: native img args
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- tools/scripts/multifine-js.sh
+++ tools/scripts/multifine-js.sh
@@ -2,10 +2,11 @@
 
 WARMUPS=200
 RUNS=200
+PROFILE=nativeOptimizedCompile
 
 hyperfine \
   --shell=none --warmup "$WARMUPS" --runs "$RUNS" \
   -n 'node' "node $@" \
   -n 'deno' "deno run $@" \
   -n 'bun' "bun run $@" \
-  -n 'elide' "./packages/cli/build/native/nativeOptimizedCompile/elide run $ELIDE_ARGS $@"
+  -n 'elide' "./packages/cli/build/native/$PROFILE/elide run --host:allow-io $ELIDE_ARGS $@"

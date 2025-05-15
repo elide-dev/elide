7d6825af69209cc700fd7d300c5bcb6c80624c42
Sam Gammon <sam@elide.dev>
2025-05-14 18:44:26 -0700

    chore: sample elide vs. gradle comparison projects
    Signed-off-by: Sam Gammon <sam@elide.dev>

---
+++ packages/cli/src/projects/kterr-gradle/src/test/kotlin/sample/testGreeting.kt
@@ -0,0 +1,11 @@
+package sample
+
+import kotlin.test.*
+
+class SampleTest {
+  @Test fun testGreeting() {
+    val greeting = render_greeting()
+    assertNotNull(greeting)
+    assertEquals("Hello, World", greeting)
+  }
+}
---
+++ packages/cli/src/projects/ktjvm-gradle/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+  kotlin("jvm") version "2.1.20"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
+    jvmTargetValidationMode.set(org.jetbrains.kotlin.gradle.dsl.jvm.JvmTargetValidationMode.WARNING)
+}
+
+dependencies {
+  implementation("com.google.guava:guava:33.4.8-jre")
+  testImplementation("org.jetbrains.kotlin:kotlin-test:2.1.20")
+}
+

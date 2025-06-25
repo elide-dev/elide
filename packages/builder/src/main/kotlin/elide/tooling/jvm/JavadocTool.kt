afa562a23b609bb80494e6ef399c2f9410d3519a
Dario Valdespino <dvaldespino00@gmail.com>
2025-06-24 22:21:05 -0400

    chore: apply linter fixes
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/builder/src/main/kotlin/elide/tooling/jvm/JavadocTool.kt
+++ packages/builder/src/main/kotlin/elide/tooling/jvm/JavadocTool.kt
@@ -10,7 +10,6 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under the License.
  */
-
 @file:Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE")
 
 package elide.tooling.jvm

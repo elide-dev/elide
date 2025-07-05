e485687667abb8baae85dc600b0356573569379d
Sam Gammon <sam@elide.dev>
2025-07-05 13:14:26 -0700

    fix: target jvm21 for packages which might be run on espresso
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- packages/exec/build.gradle.kts
+++ packages/exec/build.gradle.kts
@@ -11,6 +11,7 @@
  * License for the specific language governing permissions and limitations under the License.
  */
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import elide.internal.conventions.kotlin.KotlinTarget
 import elide.toolchain.host.TargetInfo
 
@@ -28,6 +29,7 @@ elide {
     explicitApi = true
     customKotlinCompilerArgs += listOf("-Xcontext-receivers")
   }
+
   checks {
     diktat = false
   }

2c8f773b752b0d50ad562b586c65f03b57416fc2
Sam Gammon <sam@elide.dev>
2025-07-12 12:46:31 -0700

    feat(tooling): support for mcp over http+sse
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- packages/tooling/src/main/kotlin/elide/tooling/project/mcp/ModelContextProtocol.kt
+++ packages/tooling/src/main/kotlin/elide/tooling/project/mcp/ModelContextProtocol.kt
@@ -12,6 +12,17 @@
  */
 package elide.tooling.project.mcp
 
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.install
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.EmbeddedServer
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.response.respond
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
+import io.ktor.server.sse.SSE
+import io.ktor.server.sse.sse
+import io.ktor.util.collections.ConcurrentMap
 import io.ktor.utils.io.streams.asInput
 import io.modelcontextprotocol.kotlin.sdk.Implementation
 import io.modelcontextprotocol.kotlin.sdk.ReadResourceResult
@@ -19,20 +30,90 @@ import io.modelcontextprotocol.kotlin.sdk.ServerCapabilities
 import io.modelcontextprotocol.kotlin.sdk.TextResourceContents
 import io.modelcontextprotocol.kotlin.sdk.server.Server
 import io.modelcontextprotocol.kotlin.sdk.server.ServerOptions
+import io.modelcontextprotocol.kotlin.sdk.server.SseServerTransport
 import io.modelcontextprotocol.kotlin.sdk.server.StdioServerTransport
 import io.modelcontextprotocol.kotlin.sdk.shared.AbstractTransport
 import org.graalvm.polyglot.Context
 import java.nio.charset.StandardCharsets
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.io.asSink
 import kotlinx.io.buffered
 import kotlin.io.path.readText
+import elide.runtime.Logging
 import elide.tooling.project.ElideConfiguredProject
 
 /**
  * # Model Context Protocol (MCP)
  */
 public object ModelContextProtocol {
+  /**
+   * ## MCP Serving Mode
+   *
+   * Enumerates different serving modes for the MCP server.
+   */
+  public enum class McpServingMode {
+    /**
+     * Standard I/O.
+     */
+    Stdio,
+
+    /**
+     * HTTP.
+     */
+    Http,
+  }
+
+  /**
+   * ## MCP Server Configuration
+   *
+   * Specifies types of server configurations.
+   */
+  public sealed interface McpServerConfig {
+    /**
+     * Operating mode for the MCP server.
+     */
+    public val mode: McpServingMode
+  }
+
+  /**
+   * ## MCP Server Configuration: Over Stdio
+   *
+   * This configuration uses standard I/O for the MCP server.
+   */
+  public data object McpOverStdio : McpServerConfig {
+    override val mode: McpServingMode get() = McpServingMode.Stdio
+  }
+
+  /**
+   * ## MCP Server Configuration: Over HTTP
+   *
+   * This configuration uses HTTP for the MCP server.
+   *
+   * @property host Host to bind the MCP server to.
+   * @property port Port to bind the MCP server to.
+   */
+  @JvmRecord public data class McpOverHttp internal constructor (
+    public val host: String,
+    public val port: UShort,
+  ): McpServerConfig {
+    override val mode: McpServingMode get() = McpServingMode.Http
+
+    /** Obtains instances of [McpOverHttp]. */
+    public companion object {
+      /** Default port to run MCP services on. */
+      public const val DEFAULT_MCP_HOST: String = "localhost"
+
+      /** Default port to run MCP services on. */
+      public const val DEFAULT_MCP_PORT: UShort = 8125u
+
+      /** @return [McpOverHttp] settings with the provided parameters. */
+      @JvmStatic public fun of(host: String? = null, port: UShort? = null): McpOverHttp {
+        return McpOverHttp(host ?: DEFAULT_MCP_HOST, port ?: DEFAULT_MCP_PORT)
+      }
+    }
+  }
+
   /**
    * ## MCP Configuration
    */
@@ -65,17 +146,25 @@ public object ModelContextProtocol {
     /**
      * Server which is running MCP facilities.
      */
-    public val server: Server
+    public val serverFactory: () -> Server
+
+    /**
+     * Server configuration to use.
+     */
+    public val config: McpServerConfig
 
     /**
      * Launch and Wait for MCP Server
      *
      * @param awaitClose Whether to keep the server running until interrupted.
+     * @param connect Whether to connect the server to a transport.
      * @param transport Transport to use for the MCP server.
      */
-    public suspend fun launchAndWait(awaitClose: Boolean, transport: AbstractTransport) {
+    public suspend fun launchAndWait(awaitClose: Boolean, connect: Boolean, transport: AbstractTransport) {
       try {
-        server.connect(transport)
+        if (connect) {
+          serverFactory().connect(transport)
+        }
         if (awaitClose) {
           while (true) {
             // Keep the server running until interrupted.
@@ -87,20 +176,67 @@ public object ModelContextProtocol {
       }
     }
 
+    private fun configureServer(): Server = serverFactory()
+
+    private fun httpServer(
+      config: McpOverHttp,
+      start: Boolean = true,
+      awaitClose: Boolean = true,
+    ): EmbeddedServer<*, *> {
+      val logging = Logging.of(McpServer::class)
+      val servers = ConcurrentMap<String, Server>()
+
+      return embeddedServer(CIO, host = config.host, port = config.port.toInt()) {
+        install(SSE)
+
+        routing {
+          sse("/sse") {
+            val transport = SseServerTransport("/message", this)
+            val server = configureServer()
+            servers[transport.sessionId] = server
+
+            server.onClose {
+              logging.info("Server closed")
+            }
+            server.connect(transport)
+          }
+
+          post("/message") {
+            val sessionId: String = call.request.queryParameters["sessionId"]!!
+            val transport = servers[sessionId]?.transport as? SseServerTransport
+            if (transport == null) {
+              call.respond(HttpStatusCode.NotFound, "Session not found")
+              return@post
+            }
+            transport.handlePostMessage(call)
+          }
+        }
+      }.apply {
+        if (start) {
+          logging.info("Starting MCP services over HTTP at ${config.host}:${config.port}")
+          start(wait = awaitClose)
+        }
+      }
+    }
+
     /**
      * Launch and Wait for MCP Server using standard I/O.
      *
      * @param awaitClose Whether to keep the server running until interrupted.
      */
-    public suspend fun launchAndWaitStdio(awaitClose: Boolean = true) {
-      // build and run with transport
-      val (shouldAwait, transport) = when {
-        else -> awaitClose to StdioServerTransport(
+    public suspend fun launchAndWait(config: McpServerConfig, awaitClose: Boolean = true) {
+       when (config) {
+        is McpOverStdio -> awaitClose to StdioServerTransport(
           inputStream = System.`in`.asInput().buffered(),
           outputStream = System.out.asSink().buffered(),
         )
-      }
-      return launchAndWait(shouldAwait, transport)
+        is McpOverHttp -> {
+          httpServer(config, start = awaitClose, awaitClose = awaitClose)
+          return
+        }
+      }.also { (shouldAwait, transport) ->
+         launchAndWait(shouldAwait, awaitClose, transport)
+       }
     }
   }
 
@@ -192,7 +328,8 @@ public object ModelContextProtocol {
    */
   public suspend fun McpConfiguration.build(): McpServer = buildServer(configured = project()).let { active ->
     object: McpServer {
-      override val server: Server get() = active
+      override val serverFactory: () -> Server get() = { active }
+      override val config: McpServerConfig get() = McpOverStdio
     }
   }
 }

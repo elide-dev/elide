c05de5d051e8e683881d8eb3bbadea9d82414674
Dario Valdespino <dvaldespino00@gmail.com>
2025-07-26 10:37:28 -0400

    chore(idea): linter pass
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/plugin-idea/src/main/kotlin/dev/elide/intellij/psi/ElideManifestReferencesContributor.kt
+++ packages/plugin-idea/src/main/kotlin/dev/elide/intellij/psi/ElideManifestReferencesContributor.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2024-2025 Elide Technologies, Inc.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
 package dev.elide.intellij.psi
 
 import com.intellij.codeInsight.highlighting.HighlightedReference
@@ -34,8 +46,8 @@ class ElideManifestReferencesContributor : PsiReferenceContributor() {
         .replace("IntellijIdeaRulezzz", "")
         .trim()
 
-      if (textContent.isEmpty()) return PsiReference.EMPTY_ARRAY
-      return arrayOf(ElideManifestMainClassReference(element, textContent))
+      return if (textContent.isEmpty()) PsiReference.EMPTY_ARRAY
+      else arrayOf(ElideManifestMainClassReference(element, textContent))
     }
   }
 

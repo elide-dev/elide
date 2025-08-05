e841425797f2d6d6b447d5efd0c7b6e9b3e606bb
melodicore <datafox@datafox.me>
2025-08-05 14:38:53 +0300

    feat(secrets): use DI
    Signed-off-by: melodicore <datafox@datafox.me>

--- packages/secrets/src/main/kotlin/dev/elide/secrets/impl/DataHandlerImpl.kt
+++ packages/secrets/src/main/kotlin/dev/elide/secrets/impl/DataHandlerImpl.kt
@@ -12,10 +12,7 @@
  */
 package dev.elide.secrets.impl
 
-import dev.elide.secrets.DataHandler
-import dev.elide.secrets.SecretsState
-import dev.elide.secrets.Utils
-import dev.elide.secrets.Values
+import dev.elide.secrets.*
 import dev.elide.secrets.dto.persisted.SecretCollection
 import dev.elide.secrets.dto.persisted.SecretMetadata
 import kotlinx.io.buffered
@@ -27,149 +24,126 @@ import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.readByteString
 import kotlinx.io.write
 import kotlinx.serialization.BinaryFormat
-import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.cbor.Cbor
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import kotlinx.serialization.json.Json
+import elide.annotations.Inject
+import elide.annotations.Singleton
 
 /**
  * Implementation of [DataHandler], using [Json] for metadata and [Cbor] for collections.
  *
  * @author Lauri Heino <datafox>
  */
-public class DataHandlerImpl(
-    // Using impl for visibility reasons until DI is set up
-    private val encryption: EncryptionImpl,
-    private val json: Json = Json.Default,
-    @OptIn(ExperimentalSerializationApi::class) private val cbor: BinaryFormat = Cbor.Default,
-) : DataHandler {
-    override suspend fun metadataExists(): Boolean = SystemFileSystem.exists(metadataPath())
+@Singleton
+internal class DataHandlerImpl
+@Inject
+internal constructor(private val encryption: Encryption, private val json: Json, private val cbor: BinaryFormat) :
+  DataHandler {
+  override suspend fun metadataExists(): Boolean = SystemFileSystem.exists(metadataPath())
 
-    override suspend fun readMetadata(): SecretMetadata {
-        val data = SystemFileSystem.source(metadataPath()).buffered().use { it.readByteString() }
-        return deserializeMetadata(data)
-    }
+  override suspend fun readMetadata(): SecretMetadata {
+    val data = SystemFileSystem.source(metadataPath()).buffered().use { it.readByteString() }
+    return deserializeMetadata(data)
+  }
 
-    override suspend fun writeMetadata(metadata: SecretMetadata) {
-        val data = serializeMetadata(metadata)
-        SystemFileSystem.sink(metadataPath()).buffered().use { it.write(data) }
-    }
+  override suspend fun writeMetadata(metadata: SecretMetadata) {
+    val data = serializeMetadata(metadata)
+    SystemFileSystem.sink(metadataPath()).buffered().use { it.write(data) }
+  }
 
-    override suspend fun deserializeMetadata(data: ByteString): SecretMetadata =
-        json.decodeFromString(data.decodeToString(Charsets.UTF_8))
+  override suspend fun deserializeMetadata(data: ByteString): SecretMetadata =
+    json.decodeFromString(data.decodeToString(Charsets.UTF_8))
 
-    override suspend fun serializeMetadata(metadata: SecretMetadata): ByteString =
-        json.encodeToString(metadata).encodeToByteString(Charsets.UTF_8)
+  override suspend fun serializeMetadata(metadata: SecretMetadata): ByteString =
+    json.encodeToString(metadata).encodeToByteString(Charsets.UTF_8)
 
-    override suspend fun localExists(): Boolean = SystemFileSystem.exists(localPath())
+  override suspend fun localExists(): Boolean = SystemFileSystem.exists(localPath())
 
-    override suspend fun readLocal(passphrase: ByteString): SecretCollection {
-        val encrypted = SystemFileSystem.source(localPath()).buffered().use { it.readByteString() }
-        val data = encryption.decrypt(passphrase, encrypted)
-        return cbor.decodeFromByteArray(data.toByteArray())
-    }
+  override suspend fun readLocal(passphrase: ByteString): SecretCollection {
+    val encrypted = SystemFileSystem.source(localPath()).buffered().use { it.readByteString() }
+    val data = encryption.decrypt(passphrase, encrypted)
+    return cbor.decodeFromByteArray(data.toByteArray())
+  }
 
-    override suspend fun writeLocal(passphrase: ByteString, local: SecretCollection) {
-        val data = ByteString(cbor.encodeToByteArray(local))
-        val encrypted = encryption.encrypt(passphrase, data)
-        SystemFileSystem.sink(localPath()).buffered().use { it.write(encrypted) }
-    }
+  override suspend fun writeLocal(passphrase: ByteString, local: SecretCollection) {
+    val data = ByteString(cbor.encodeToByteArray(local))
+    val encrypted = encryption.encrypt(passphrase, data)
+    SystemFileSystem.sink(localPath()).buffered().use { it.write(encrypted) }
+  }
 
-    override suspend fun collectionExists(profile: String): Boolean =
-        SystemFileSystem.exists(collectionPath(profile))
+  override suspend fun collectionExists(profile: String): Boolean = SystemFileSystem.exists(collectionPath(profile))
 
-    override suspend fun readCollection(profile: String, passphrase: ByteString): SecretCollection {
-        val key = readKey(profile, passphrase)
-        val encrypted = readEncryptedCollection(profile)
-        return decryptCollection(key, encrypted)
-    }
+  override suspend fun readCollection(profile: String, passphrase: ByteString): SecretCollection {
+    val key = readKey(profile, passphrase)
+    val encrypted = readEncryptedCollection(profile)
+    return decryptCollection(key, encrypted)
+  }
 
-    override suspend fun readEncryptedCollection(profile: String): ByteString =
-        SystemFileSystem.source(collectionPath(profile)).buffered().use { it.readByteString() }
+  override suspend fun readEncryptedCollection(profile: String): ByteString =
+    SystemFileSystem.source(collectionPath(profile)).buffered().use { it.readByteString() }
 
-    override suspend fun writeCollection(
-        profile: String,
-        passphrase: ByteString,
-        collection: SecretCollection,
-    ): String {
-        val key = readKey(profile, passphrase)
-        val encrypted = encryptCollection(key, collection)
-        SystemFileSystem.sink(collectionPath(profile)).buffered().use { it.write(encrypted) }
-        return Utils.sha(encrypted)
-    }
+  override suspend fun writeCollection(profile: String, passphrase: ByteString, collection: SecretCollection): String {
+    val key = readKey(profile, passphrase)
+    val encrypted = encryptCollection(key, collection)
+    SystemFileSystem.sink(collectionPath(profile)).buffered().use { it.write(encrypted) }
+    return Utils.sha(encrypted)
+  }
 
-    override suspend fun deleteCollection(profile: String) {
-        SystemFileSystem.delete(collectionPath(profile))
-        SystemFileSystem.delete(keyPath(profile))
-    }
+  override suspend fun deleteCollection(profile: String) {
+    SystemFileSystem.delete(collectionPath(profile))
+    SystemFileSystem.delete(keyPath(profile))
+  }
 
-    override suspend fun decryptCollection(
-        key: ByteString,
-        encrypted: ByteString,
-    ): SecretCollection {
-        val data = encryption.decrypt(key, encrypted)
-        return cbor.decodeFromByteArray(data.toByteArray())
-    }
+  override suspend fun decryptCollection(key: ByteString, encrypted: ByteString): SecretCollection {
+    val data = encryption.decrypt(key, encrypted)
+    return cbor.decodeFromByteArray(data.toByteArray())
+  }
 
-    override suspend fun encryptCollection(
-        key: ByteString,
-        collection: SecretCollection,
-    ): ByteString {
-        val data = ByteString(cbor.encodeToByteArray(collection))
-        return encryption.encrypt(key, data)
-    }
+  override suspend fun encryptCollection(key: ByteString, collection: SecretCollection): ByteString {
+    val data = ByteString(cbor.encodeToByteArray(collection))
+    return encryption.encrypt(key, data)
+  }
 
-    override suspend fun keyExists(profile: String): Boolean =
-        SystemFileSystem.exists(keyPath(profile))
+  override suspend fun keyExists(profile: String): Boolean = SystemFileSystem.exists(keyPath(profile))
 
-    override suspend fun readKey(profile: String, passphrase: ByteString): ByteString {
-        val encrypted =
-            SystemFileSystem.source(keyPath(profile)).buffered().use { it.readByteString() }
-        return decryptKey(passphrase, encrypted)
-    }
+  override suspend fun readKey(profile: String, passphrase: ByteString): ByteString {
+    val encrypted = SystemFileSystem.source(keyPath(profile)).buffered().use { it.readByteString() }
+    return decryptKey(passphrase, encrypted)
+  }
 
-    override suspend fun writeKey(profile: String, passphrase: ByteString, key: ByteString) {
-        val encrypted = encryptKey(passphrase, key)
-        SystemFileSystem.sink(keyPath(profile)).buffered().use { it.write(encrypted) }
-    }
+  override suspend fun writeKey(profile: String, passphrase: ByteString, key: ByteString) {
+    val encrypted = encryptKey(passphrase, key)
+    SystemFileSystem.sink(keyPath(profile)).buffered().use { it.write(encrypted) }
+  }
 
-    override suspend fun decryptKey(passphrase: ByteString, encrypted: ByteString): ByteString {
-        return encryption.decrypt(passphrase, encrypted)
-    }
+  override suspend fun decryptKey(passphrase: ByteString, encrypted: ByteString): ByteString {
+    return encryption.decrypt(passphrase, encrypted)
+  }
 
-    override suspend fun encryptKey(passphrase: ByteString, key: ByteString): ByteString {
-        return encryption.encrypt(passphrase, key)
-    }
+  override suspend fun encryptKey(passphrase: ByteString, key: ByteString): ByteString {
+    return encryption.encrypt(passphrase, key)
+  }
 
-    override suspend fun createValidator(
-        metadata: SecretMetadata,
-        passphrase: ByteString,
-    ): ByteString {
-        val data = encryption.hash(metadata.name + metadata.organization)
-        val encrypted = encryption.encrypt(passphrase, data)
-        return encrypted
-    }
+  override suspend fun createValidator(metadata: SecretMetadata, passphrase: ByteString): ByteString {
+    val data = encryption.hash(metadata.name + metadata.organization)
+    val encrypted = encryption.encrypt(passphrase, data)
+    return encrypted
+  }
 
-    override suspend fun validate(
-        metadata: SecretMetadata,
-        passphrase: ByteString,
-        validator: ByteString,
-    ): Boolean {
-        val expected = encryption.hash(metadata.name + metadata.organization)
-        val actual = encryption.decrypt(passphrase, validator)
-        return expected == actual
-    }
+  override suspend fun validate(metadata: SecretMetadata, passphrase: ByteString, validator: ByteString): Boolean {
+    val expected = encryption.hash(metadata.name + metadata.organization)
+    val actual = encryption.decrypt(passphrase, validator)
+    return expected == actual
+  }
 
-    private suspend fun metadataPath(): Path =
-        Path(SecretsState.Companion.get().path, Values.METADATA_NAME)
+  private suspend fun metadataPath(): Path = Path(SecretsState.Companion.get().path, Values.METADATA_NAME)
 
-    private suspend fun localPath(): Path =
-        Path(SecretsState.Companion.get().path, Values.LOCAL_COLLECTION_NAME)
+  private suspend fun localPath(): Path = Path(SecretsState.Companion.get().path, Values.LOCAL_COLLECTION_NAME)
 
-    private suspend fun collectionPath(profile: String): Path =
-        Path(SecretsState.Companion.get().path, Utils.collectionName(profile))
+  private suspend fun collectionPath(profile: String): Path =
+    Path(SecretsState.Companion.get().path, Utils.collectionName(profile))
 
-    private suspend fun keyPath(profile: String): Path =
-        Path(SecretsState.Companion.get().path, Utils.keyName(profile))
+  private suspend fun keyPath(profile: String): Path = Path(SecretsState.Companion.get().path, Utils.keyName(profile))
 }

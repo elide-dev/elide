db5e9cf64599785e1308f20be50187d38dda6b3d
Dario Valdespino <dvaldespino00@gmail.com>
2025-09-08 12:14:56 -0400

    chore: code cleanup and linting

--- packages/builder/src/main/kotlin/elide/tooling/testing/jvm/GuestJvmTestDriver.kt
+++ packages/builder/src/main/kotlin/elide/tooling/testing/jvm/GuestJvmTestDriver.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2024-2025 Elide Technologies, Inc.
+ *
+ * Licensed under the MIT license (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   https://opensource.org/license/mit/
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the License.
+ */
 package elide.tooling.testing.jvm
 
 import elide.runtime.core.PolyglotContext
@@ -14,11 +26,10 @@ public class GuestJvmTestDriver(private val contextProvider: () -> PolyglotConte
   override val type: TestTypeKey<JvmTestCase> get() = JvmTestCase
 
   override suspend fun run(testCase: JvmTestCase): TestOutcome {
-    val guestContext = contextProvider()
-    val testClass = guestContext.bindings(Jvm.Plugin).getMember(testCase.className)
+    val testClass = contextProvider().bindings(Jvm.Plugin).getMember(testCase.className)
       ?: return TestOutcome.Error("Failed to resolve test class: ${testCase.className}")
 
-    if (!testClass.canInstantiate()) return TestOutcome.Error("Cannot instantiate test class ${testCase.className}")
+    if (!testClass.canInstantiate()) return TestOutcome.Error("Failed to instantiate test class ${testCase.className}")
     val testInstance = testClass.newInstance()
 
     if (!testInstance.canInvokeMember(testCase.methodName))
@@ -26,7 +37,7 @@ public class GuestJvmTestDriver(private val contextProvider: () -> PolyglotConte
 
     return runCatching { testInstance.invokeMember(testCase.methodName) }.fold(
       onSuccess = { TestOutcome.Success },
-      onFailure = { TestOutcome.Failure(it) }
+      onFailure = { TestOutcome.Failure(it) },
     )
   }
 }

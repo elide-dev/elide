bf2c85bf18d7a3c134a3e370d8693a4fdd99c202
Sam Gammon <sam@elide.dev>
2025-09-21 12:50:48 -0700

    fix: updated jvm lib version misalignment
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- packages/tooling/src/main/kotlin/elide/tooling/jvm/JvmLibraries.kt
+++ packages/tooling/src/main/kotlin/elide/tooling/jvm/JvmLibraries.kt
@@ -22,7 +22,7 @@ import elide.tooling.Classpath
  */
 public object JvmLibraries {
   // @TODO: don't hard-code any of this
-  public const val ELIDE_VERSION: String = "1.0.0-beta8"
+  public const val ELIDE_VERSION: String = "1.0.0-beta9"
   public const val EMBEDDED_KOTLIN: String = "2.2.20"
 
   public const val EMBEDDED_JUNIT_VERSION: String = "5.13.1"
@@ -34,7 +34,7 @@ public object JvmLibraries {
   public const val EMBEDDED_KOTLINX_IO_VERSION: String = "0.8.0"
   public const val EMBEDDED_COROUTINES_VERSION: String = "1.10.2"
   public const val EMBEDDED_SERIALIZATION_VERSION: String = "1.9.0"
-  public const val EMBEDDED_REDACTED_VERSION: String = "1.14.1"
+  public const val EMBEDDED_REDACTED_VERSION: String = "1.15.0"
   public const val APIGUARDIAN_API: String = "org.apiguardian:apiguardian-api"
   public const val JUNIT_JUPITER_API: String = "org.junit.jupiter:junit-jupiter-api"
   public const val JUNIT_JUPITER_ENGINE: String = "org.junit.jupiter:junit-jupiter-engine"

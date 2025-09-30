c7bfa9ed033a7aba3d44997f802268c89559272a
Sam Gammon <sam@elide.dev>
2025-09-29 22:49:36 -0700

    chore: sample project for flask integration
    Signed-off-by: Sam Gammon <sam@elide.dev>

---
+++ packages/cli/src/projects/flask/app.py
@@ -0,0 +1,14 @@
+from elide import Flask
+
+# create and configure an app
+app = Flask(__name__)
+
+# basic app routing
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+print(f"Starting server worker at http://localhost:3000")
+
+# start the server, required for now
+app.bind()

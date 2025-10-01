29dd2eed31f6449a563515573d439d385ef7119f
Dario Valdespino <dvaldespino00@gmail.com>
2025-10-01 19:19:18 -0400

    chore: add flask-react project sample
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

---
+++ packages/cli/src/projects/flask-react/app.py
@@ -0,0 +1,13 @@
+from elide import Flask, react, request
+
+# create and configure an app
+app = Flask(__name__)
+home = react("index.tsx")
+
+# basic app routing
+@app.route("/")
+def hello_world():
+    user_agent = request.headers["User-Agent"]
+    return home(user_agent = user_agent)
+
+print("Starting server worker at http://localhost:3000")

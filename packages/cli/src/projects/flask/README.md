c2c2650d0e54783459e0871e2f4f1b93ab458ca0
Sam Gammon <sam@elide.dev>
2025-10-01 11:42:18 -0700

    chore: gifs/sample cleanup for flask
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- packages/cli/src/projects/flask/README.md
+++ packages/cli/src/projects/flask/README.md
@@ -1,3 +1,6 @@
 # elide flask
 
-Sample Elide application using the Flask framework.
+Sample Elide application using the Flask framework. Run the app with:
+```
+elide run app.py
+```
--- packages/cli/src/projects/flask/app.py
+++ packages/cli/src/projects/flask/app.py
@@ -1,14 +1,7 @@
 from elide import Flask
 
-# create and configure an app
 app = Flask(__name__)
 
-# basic app routing
 @app.route("/")
 def hello_world():
   return "<p>Hello, World!</p>"
-
-print(f"Starting server worker at http://localhost:3000")
-
-# start the server, required for now
-app.bind()

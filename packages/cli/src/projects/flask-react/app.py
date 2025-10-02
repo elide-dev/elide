0dd19666c65c452be38ffb9362854ebf33b8dde1
Dario Valdespino <dvaldespino00@gmail.com>
2025-10-01 22:16:23 -0400

    fix: null-safe header access in flask react sample
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/cli/src/projects/flask-react/app.py
+++ packages/cli/src/projects/flask-react/app.py
@@ -7,7 +7,7 @@ home = react("index.tsx")
 # basic app routing
 @app.route("/")
 def hello_world():
-    user_agent = request.headers["User-Agent"]
+    user_agent = request.headers.get("User-Agent")
     return home(user_agent = user_agent)
 
 print("Starting server worker at http://localhost:3000")

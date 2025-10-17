b10f0e641634e6d02f3ddf9f142b02a6828a4a62
Francis Follis <hi@francis.fyi>
2025-10-17 13:55:26 +0800

    initial skeleton for db studio command

--- packages/cli/src/main/kotlin/elide/tool/cli/Elide.kt
+++ packages/cli/src/main/kotlin/elide/tool/cli/Elide.kt
@@ -44,6 +44,7 @@ import elide.tool.cli.cfg.ElideCLITool.ELIDE_TOOL_VERSION
 import elide.tool.cli.cmd.builder.ToolBuildCommand
 import elide.tool.cli.cmd.builder.ToolWhichCommand
 import elide.tool.cli.cmd.classpath.ClasspathCommand
+import elide.tool.cli.cmd.db.DbCommand
 import elide.tool.cli.cmd.deps.AddCommand
 import elide.tool.cli.cmd.deps.InstallCommand
 import elide.tool.cli.cmd.dev.DevCommand
@@ -138,6 +139,7 @@ internal const val ELIDE_HEADER = ("@|bold,fg(magenta)%n" +
     Elide.Completions::class,
     ToolSecretsCommand::class,
     ToolS3Command::class,
+    DbCommand::class,
   ],
   customSynopsis = [
     "",

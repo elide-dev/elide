115084a6fd0214f840ffff7ee0bc0bc29b644742
Boris Ryabov <stonks33@gmail.com>
2025-10-18 01:24:22 -0700

    [REFACTOR] breaking up the pipeline
    PRE

--- tools/scripts/release-old/build-publish-release.sh
+++ tools/scripts/release-old/build-publish-release.sh
@@ -16,6 +16,7 @@ publish=false
 release=false
 
 stamp=false
+clean=false
 pgo=false
 provenance=false
 help=false
@@ -30,6 +31,7 @@ publishTask=publishAllPublicationsToStageRepository
 version=""
 NPM_ARGS=""
 GRADLE_PROPS=""
+PACKAGE_ARGS=""
 buildMode=dev
 
 if [[ "$@" == *"--help"* ]]; then
@@ -52,13 +54,23 @@ if [[ "$help" == true ]]; then
   exit 0
 fi
 
+if [["$@" == *"--dry"*]]; then
+  dry=true
+fi
+
 if [[ "$@" == *"--publish"* ]]; then
   publish=true
   release=true
+  dry=false
 fi
 
  if [[ "$@" == *"--release"* ]]; then
    release=true
+   clean=true
+fi
+
+if [[ "$@" == *"--version"* ]]; then
+  version=$(echo "$@" | grep -oP '(?<=--version\s)[^\s]+')
 fi
 
 if [[ "$release" == true ]]; then
@@ -78,11 +90,13 @@ fi
 # Build Options, NPM and Gradle
 if [[ "$provenance" == true ]]; then
   NPM_ARGS="$NPM_ARGS --provenance"
+  PACKAGE_ARGS="$PACKAGE_ARGS --provenance"
 fi
 
 # in release mode, tell gradle to build the docs and enable signing
 if [[ "$release" == true ]]; then
   GRADLE_PROPS="$GRADLE_PROPS -Pelide.buildDocs=true -PenableSigning=true"
+  PACKAGE_ARGS="$PACKAGE_ARGS --release --stamp"
   if [[ "$dry" != true ]]; then
     GRADLE_PROPS="$GRADLE_PROPS -PenableSigstore=true"
   fi
@@ -95,6 +109,17 @@ ELIDE_MAVEN_REPO="elide:elide-maven"
 GRADLE_PROPS="-Pelide.release=$release -Pelide.buildMode=$buildMode -Pelide.stamp=$stamp -Pelide.pgo=$pgo $GRADLE_PROPS"
 GRADLE_ARGS="--no-configuration-cache"
 PUBLISH_TARGETS=":packages:core:$publishTask :packages:base:$publishTask :packages:test:$publishTask :packages:tooling:$publishTask :packages:builder:$publishTask :packages:graalvm:$publishTask :packages:server:$publishTask :packages:http:$publishTask :packages:graalvm-java:$publishTask :packages:graalvm-js:$publishTask :packages:graalvm-jvm:$publishTask :packages:graalvm-kt:$publishTask :packages:graalvm-llvm:$publishTask :packages:graalvm-py:$publishTask :packages:graalvm-rb:$publishTask :packages:graalvm-ts:$publishTask :packages:graalvm-wasm:$publishTask :packages:engine:$publishTask"
+
+## Perform a clean if needed
+if [[ "$clean" == true ]]; then
+  echo "Cleaning previous build artifacts..."
+  set -x;
+  ./gradlew \
+    $GRADLE_ARGS \
+    $GRADLE_PROPS \
+    clean;
+  set +x;
+fi
 
 ## Assemble libraries
 set -x;
@@ -119,7 +144,78 @@ export ELIDE_VERSION="$version";
 export ELIDE_PLATFORM="$platform";
 export ELIDE_ARCH="$arch";
 
+source ./tools/scripts/release/publish-release.sh --version=$version $PACKAGE_ARGS
 
+if [[ "$dry" != true ]]; then
+  set -x;
+  cd ./packages/cli/build/native/nativeOptimizedCompile/;
+  mkdir -p "release/${platform}-${arch}/$version;"
+  cp -fv elide-*.{tgz,txz,zip}* "release/${platform}-${arch}/$version/"
+  cp -fv elide.sbom.json "release/${platform}-${arch}/$version/"
+  cp -fv elide-build-report.html "release/${platform}-${arch}/$version/elide.build-report.html"
+  cd -;
+  mkdir -p staging/release
+  mv "packages/cli/build/native/nativeOptimizedCompile/release" "staging/"
+  tree -L 3 staging/
+  echo "Release built."
+  set +x;
+fi
+
+## Build build/m2 root into a zip
+if [[ "$publish" == true ]]; then
+  echo "Building m2 zip..."
+  cd build/m2;
+  set -x;
+  rm -fv ../elide-m2.zip && zip -r ../elide-m2.zip . || {
+    echo "Failed to create m2 zip. Exiting."
+    exit 1
+  };
+  set +x;
+  cd -;
+fi
+
+## I THINK??
+## if --publish is active, run rclone
+if [[ "$publish" == true ]]; then
+  echo "Publishing Maven artifacts to Elide repository..."
+  if [[ "$dry" == true ]]; then
+    echo "Dry run: would publish to remote repository."
+    echo cd build/m2 && echo rclone copy --progress . $ELIDE_MAVEN_REPO/
+    echo "Dry run: would publish npm types."
+    cd packages/types && npm publish --access public --dry $NPM_ARGS;
+  else
+    set -x;
+    cd build/m2 && rclone copy --progress . $ELIDE_MAVEN_REPO/
+    set +x;
+    echo "Elide Maven updated."
+    set -x;
+    cd packages/types && npm publish --access public $NPM_ARGS;
+    set +x;
+    echo "Elide NPM libs updated."
+  fi
+else
+  echo "Skipping publishing step."
+fi
+
+echo "Release build completed successfully."
+
+echo "┌─────────────────────────────────────────────────┐"
+echo "╵ Elide release receipt                           ╵"
+echo "└─────────────────────────────────────────────────┘"
+echo "- Version: $version"
+echo "- Platform: $platform"
+echo "- Architecture: $arch"
+echo "- Tag: ${platform}-${arch}"
+echo "- Mode: $buildMode"
+echo "- Release: $release"
+echo "- Stamp: $stamp"
+echo "- Dry run: $dry"
+echo "- Publish: $publish"
+echo "- Provenance: $provenance"
+echo "- LibC: $libc"
+echo "- PGO: $pgo"
+echo "- Clean: $clean"
+echo "- Third-party: $thirdParty"
 
 echo ""
 echo "Done."

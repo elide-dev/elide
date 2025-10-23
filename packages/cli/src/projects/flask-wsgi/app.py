f9bc97663fe108ebdd62389fa50fc48ad2675e33
Dario Valdespino <dvaldespino00@gmail.com>
2025-10-23 19:48:55 -0400

    feat: wsgi-flask-react sample
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/cli/src/projects/flask-wsgi/app.py
+++ packages/cli/src/projects/flask-wsgi/app.py
@@ -1,13 +1,21 @@
 from flask import Flask, request
+from elide import react
+
 
 def make_app(server_name: str, server_version: str):
   print("Starting app worker")
   app = Flask(__name__)
 
+  home = react("index.tsx")
   base_headers = { "Server": f"{server_name} {server_version}" }
 
   @app.route("/")
-  def hello_world():
+  def root():
+    user_agent = request.headers.get("User-Agent", "<not set>")
+    return (home(user_agent = user_agent), base_headers)
+
+  @app.get("/hello")
+  def hello():
     return ("<p>Hello, World!</p>", base_headers)
 
   @app.post("/echo")

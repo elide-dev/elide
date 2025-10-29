f3c31ecb5a6f4d584b35c978ac4e599a624d20b6
Francis Follis <hi@francis.fyi>
2025-10-30 02:00:43 +0800

    feat(database-ui): add shadcn and tailwind to studio frontend

---
+++ packages/cli/src/projects/db-studio/ui/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
+  },
+  plugins: [],
+}

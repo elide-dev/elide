f166b7d2e6220f3d43236f5de448b6c7dd46c326
Elijah Kotyluk-Butler <elijah@Elijahs-MacBook-Pro.local>
2025-10-30 04:23:05 -0500

    feat(randomInt): add randomInt method.
    Signed-off-by: Elijah Kotyluk <elijah@elijahkotyluk.com>

--- packages/graalvm/src/main/kotlin/elide/runtime/node/crypto/NodeCrypto.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/node/crypto/NodeCrypto.kt
@@ -16,6 +16,7 @@ import org.graalvm.polyglot.Value
 import org.graalvm.polyglot.proxy.ProxyExecutable
 import elide.runtime.gvm.api.Intrinsic
 import elide.runtime.gvm.internals.intrinsics.js.AbstractNodeBuiltinModule
+import elide.runtime.intrinsics.js.err.JsError
 import elide.runtime.gvm.js.JsSymbol.JsSymbols.asJsSymbol
 import elide.runtime.gvm.loader.ModuleInfo
 import elide.runtime.gvm.loader.ModuleRegistry
@@ -24,12 +25,15 @@ import elide.runtime.intrinsics.GuestIntrinsic.MutableIntrinsicBindings
 import elide.runtime.intrinsics.js.node.CryptoAPI
 import elide.runtime.lang.javascript.NodeModuleName
 import elide.vm.annotations.Polyglot
+import java.security.SecureRandom
 
 // Internal symbol where the Node built-in module is installed.
 private const val CRYPTO_MODULE_SYMBOL = "node_${NodeModuleName.CRYPTO}"
 
-// Functiopn name for randomUUID
+// Function name for randomUUID
 private const val F_RANDOM_UUID = "randomUUID"
+private const val F_RANDOM_INT = "randomInt"
+
 
 // Installs the Node crypto module into the intrinsic bindings.
 @Intrinsic internal class NodeCryptoModule : AbstractNodeBuiltinModule() {
@@ -54,6 +58,7 @@ internal class NodeCrypto private constructor () : ReadOnlyProxyObject, CryptoAP
     // Module members
     private val moduleMembers = arrayOf(
       F_RANDOM_UUID,
+      F_RANDOM_INT,
     ).apply { sort() }
   }
 
@@ -62,6 +67,28 @@ internal class NodeCrypto private constructor () : ReadOnlyProxyObject, CryptoAP
     // Note `options` parameter exists for Node.js compatibility but is currently ignored
     // It supports { disableEntropyCache: boolean } which is not applicable to our implementation
     return java.util.UUID.randomUUID().toString()
+  }
+
+  @Polyglot override fun randomInt(
+    min: Int,
+    max: Int,
+    callback: ((JsError, Int) -> Unit)?
+  ): Int {
+    require(min < max) { "min must be less than max" }
+
+    if (max - min <= 0) {
+      return min
+    }
+
+    val randomIntValue = SecureRandom().nextInt(max - min)
+
+    return min + randomIntValue
+  }
+
+  override fun randomInt(
+    max: Int
+  ): Int {
+    return randomInt(min = 0, max)
   }
 
   // ProxyObject implementation
@@ -75,6 +102,33 @@ internal class NodeCrypto private constructor () : ReadOnlyProxyObject, CryptoAP
       // Node.js signature: randomUUID([options])
       val options = args.getOrNull(0)
       randomUUID(options)
+    }
+    F_RANDOM_INT -> ProxyExecutable { args ->
+      // Node.js signature: randomInt(min, max, [callback]) OR randomInt(max, [callback])
+      when (args.size) {
+        1 -> {
+          val max = args[0].asInt()
+          randomInt(max)
+        }
+        2 -> {
+          val min = args[0].asInt()
+          val max = args[1].asInt()
+          randomInt(min, max)
+        }
+        3 -> {
+          val min = args[0].asInt()
+          val max = args[1].asInt()
+          val callback = args[2]
+          if (callback.canExecute()) {
+            return@ProxyExecutable randomInt(min, max) .also { result ->
+              callback.execute(null, result)
+            }
+          } else {
+            throw IllegalArgumentException("Callback provided to randomInt is not executable")
+          }
+        }
+        else -> throw IllegalArgumentException("randomInt expects 1 to 3 arguments, got ${args.size}")
+      }
     }
     else -> null
   }

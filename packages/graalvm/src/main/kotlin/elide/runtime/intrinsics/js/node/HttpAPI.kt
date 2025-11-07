a988dc649057f9d39b2810e7776be4d33274efa7
Dario Valdespino <dvaldespino00@gmail.com>
2025-11-06 22:24:04 -0500

    chore: apply linter fixes
    Signed-off-by: Dario Valdespino <dvaldespino00@gmail.com>

--- packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/node/HttpAPI.kt
+++ packages/graalvm/src/main/kotlin/elide/runtime/intrinsics/js/node/HttpAPI.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Elide Technologies, Inc.
+ * Copyright (c) 2024-2025 Elide Technologies, Inc.
  *
  * Licensed under the MIT license (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
